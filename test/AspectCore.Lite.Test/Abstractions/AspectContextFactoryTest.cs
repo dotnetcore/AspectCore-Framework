1ca82b29cf432b5103eb22d91148033ec59e7149
liuhaoyang <liuhaoyang1221@hotmail.com>
2016-10-11 13:16:25 +0800

    add test/AspectCore.Lite.Test/Generators/InterfaceProxyGeneratorTest

---
+++ test/AspectCore.Lite.Test/Abstractions/AspectContextFactoryTest.cs
@@ -0,0 +1,34 @@
+﻿using AspectCore.Lite.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AspectCore.Lite.Test.Abstractions
+{
+    public class AspectContextFactoryTest
+    {
+        private readonly IServiceProvider serviceProvider;
+        public AspectContextFactoryTest()
+        {
+            serviceProvider = DependencyResolver.GetServiceProvider();
+        }
+
+        [Fact]
+        public void AspectContextFactory_Create_Test()
+        {
+            var aspectContextFactory = serviceProvider.GetService<IAspectContextFactory>();
+            Assert.NotNull(aspectContextFactory);
+            var aspectContext = aspectContextFactory.Create();
+            Assert.NotNull(aspectContext);
+            Assert.Equal(serviceProvider, aspectContext.ApplicationServices);
+            Assert.NotNull(aspectContext.AspectServices);
+            Assert.Null(aspectContext.Parameters);
+            Assert.Null(aspectContext.Proxy);
+            Assert.Null(aspectContext.ReturnParameter);
+            Assert.Null(aspectContext.Target);
+        }
+    }
+}
---
+++ test/AspectCore.Lite.Test/Generators/InterfaceProxyGeneratorTest.cs
@@ -0,0 +1,64 @@
+﻿using AspectCore.Lite.Generators;
+using Microsoft.AspNetCore.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AspectCore.Lite.Test.Generators
+{
+    public class InterfaceProxyGeneratorTest
+    {
+        private readonly IServiceProvider serviceProvider;
+        public InterfaceProxyGeneratorTest()
+        {
+            serviceProvider = DependencyResolver.GetServiceProvider();
+        }
+
+        [Fact]
+        public void InterfaceProxyGenerator_Constructor_Test()
+        {
+            var interfaceProxyGenerator = new InterfaceProxyGenerator(serviceProvider, typeof(IInterfaceProxyGeneratorTest));
+            Assert.NotNull(interfaceProxyGenerator);
+            ExceptionAssert.ThrowsArgumentNull(() => new InterfaceProxyGenerator(null, typeof(IInterfaceProxyGeneratorTest)), "serviceProvider");
+            ExceptionAssert.ThrowsArgumentNull(() => new InterfaceProxyGenerator(serviceProvider, null), "interfaceType");
+            ExceptionAssert.ThrowsArgument(() => new InterfaceProxyGenerator(serviceProvider, typeof(object)), "interfaceType", "Type should be interface.");
+        }
+
+        [Fact]
+        public void InterfaceProxyGenerator_TypeBuilder_Throw_Test()
+        {
+            var interfaceProxyGenerator = new InterfaceProxyGenerator(serviceProvider, typeof(IInterfaceProxyGeneratorTest));
+            ExceptionAssert.Throws<InvalidOperationException>(() => interfaceProxyGenerator.TypeBuilder, $"The proxy of {typeof(IInterfaceProxyGeneratorTest).FullName} is not generated.");
+        }
+
+        [Fact]
+        public void InterfaceProxyGenerator_GenerateProperty_Test()
+        {
+            var interfaceProxyGenerator = new InterfaceProxyGenerator(serviceProvider, typeof(IInterfaceProxyGeneratorTest));
+            var type = interfaceProxyGenerator.GenerateProxyType();
+            var instance = (IInterfaceProxyGeneratorTest)Activator.CreateInstance(type, serviceProvider, new InterfaceProxyGeneratorProxy());
+            instance.Id = "proxy";
+            Assert.Equal(instance.Id, "proxy");
+            Assert.Equal(instance.GetName("Test"), "proxyTest");
+        }
+    }
+
+    public interface IInterfaceProxyGeneratorTest
+    {
+        string Id { get; set; }
+
+        string GetName(string name);
+    }
+
+    public class InterfaceProxyGeneratorProxy : IInterfaceProxyGeneratorTest
+    {
+        public string Id { get; set; }
+
+        public string GetName(string name)
+        {
+            return Id + name;
+        }
+    }
+}
