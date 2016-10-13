7d29b53483ce8c2e794848e6dd48c1b0690d5bbe
liuhaoyang <liuhaoyang1221@hotmail.com>
2016-10-14 00:59:39 +0800

    add AspectCore.Lite.Autofac

---
+++ src/AspectCore.Lite.Autofac/ContainerBuilderExtensions.cs
@@ -0,0 +1,49 @@
+﻿using AspectCore.Lite.Generators;
+using Autofac;
+using Autofac.Core;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace AspectCore.Lite.Autofac
+{
+    public static class ContainerBuilderExtensions
+    {
+        public static ContainerBuilder RegisterAspectLite(this ContainerBuilder containerBuilder , IEnumerable<ServiceDescriptor> aspectServices)
+        {
+            if (containerBuilder == null)
+            {
+                throw new ArgumentNullException(nameof(containerBuilder));
+            }
+
+            containerBuilder.Populate(aspectServices);
+            containerBuilder.RegisterCallback(registry => registry.Registered += Registry_Registered);
+
+            return containerBuilder;
+        }
+
+        private static void Registry_Registered(object sender , ComponentRegisteredEventArgs args)
+        {
+            args.ComponentRegistration.Activating += ComponentRegistration_Activating;
+        }
+
+        private static void ComponentRegistration_Activating(object sender , ActivatingEventArgs<object> args)
+        {
+            var serviceProvider = args.Context.Resolve<IServiceProvider>();
+            var serviceTypes = args.Component.Services.OfType<IServiceWithType>().Select(s => s.ServiceType);
+            var interfaceTypes = serviceTypes.Where(type => type.GetTypeInfo().IsInterface);
+            var classCount = serviceTypes.Count(type => type.GetTypeInfo().IsClass);
+            if (classCount == 0)
+            {
+
+                var interfaceProxyGenerator = new InterfaceProxyGenerator(serviceProvider , interfaceTypes.First() , interfaceTypes.Skip(1).ToArray());
+                var proxyType = interfaceProxyGenerator.GenerateProxyType();
+                var proxyInstance = ActivatorUtilities.CreateInstance(serviceProvider , proxyType , serviceProvider , args.Instance);
+                args.Instance = proxyInstance;
+            }
+        }
+    }
+}
