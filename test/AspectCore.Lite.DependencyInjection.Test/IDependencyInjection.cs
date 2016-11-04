003382a6706a3a1ac6f05dd2acb57e0067055594
liuhaoyang <liuhaoyang1221@hotmail.com>
2016-11-04 22:35:26 +0800

    init AspectCore.Lite.DependencyInjection.Test

---
+++ test/AspectCore.Lite.DependencyInjection.Test/IDependencyInjection.cs
@@ -0,0 +1,23 @@
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using AspectCore.Lite.DependencyInjection;
+
+namespace AspectCore.Lite.DependencyInjection.Test
+{
+    public interface IDependencyInjection
+    {
+    }
+
+     public static class IDependencyInjectionExtensions
+    {
+        public static IServiceProvider BuildServiceProvider(this IDependencyInjection di , Action<IServiceCollection> action = null)
+        {
+            IServiceCollection services = new ServiceCollection();
+
+            services.AddAspectLite();
+            action?.Invoke(services);
+
+            return services.BuildServiceProvider();
+        }
+    }
+}
