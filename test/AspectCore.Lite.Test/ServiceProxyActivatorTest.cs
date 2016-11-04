fa21f5eef1fb633e18a6ce7fecc1dc52ac713160
liuhaoyang <liuhaoyang1221@hotmail.com>
2016-11-04 22:34:48 +0800

    Add ServiceProxyActivator Test

---
+++ test/AspectCore.Lite.Test/ServiceProxyActivatorTest.cs
@@ -0,0 +1,17 @@
+﻿using AspectCore.Lite.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace AspectCore.Lite.Test
+{
+    public class ServiceProxyActivatorTest:IDependencyInjection
+    {
+        [Fact]
+        public void ServiceProxyActivator_Test()
+        {
+            var serviceProvider = this.BuildServiceProvider();
+            var proxyActivator = serviceProvider.GetRequiredService<IProxyActivator>();
+            Assert.IsAssignableFrom<IProxyActivator>(proxyActivator);
+        }
+    }
+}
