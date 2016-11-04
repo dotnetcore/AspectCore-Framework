fafbcf7e7e5c4d01d6454112c7f7d81335976d6f
liuhaoyang <liuhaoyang1221@hotmail.com>
2016-11-04 20:48:48 +0800

    add AspectCore.Lite.DependencyInjection.Test

---
+++ test/AspectCore.Lite.DependencyInjection.Test/Tests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xunit;
+
+namespace AspectCore.Lite.DependencyInjection.Test
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
+
+
