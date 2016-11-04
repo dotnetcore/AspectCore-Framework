d22b01545de8b7cb6844501ccafb8c3b4c3db23e
liuhaoyang <liuhaoyang1221@hotmail.com>
2016-11-04 23:13:36 +0800

    test AspectServiceProviderFactory passed

--- src/AspectCore.Lite.DependencyInjection/AspectServiceProviderFactory.cs
+++ src/AspectCore.Lite.DependencyInjection/AspectServiceProviderFactory.cs
@@ -5,6 +5,18 @@ namespace AspectCore.Lite.DependencyInjection
 {
     public static class AspectServiceProviderFactory
     {
+        /// <summary>
+        /// Create Aspect Proxy ServiceProvider.
+        /// Before call Create method , must call the IServiceCollection.AddAspectLite extension method to add the AspectLiteServices to the serviceCollection.
+        /// </summary>
+        /// <param name="serviceProvider">original serviceProvider</param>
+        /// <exception cref="InvalidOperationException">There is no service of AspectLite.</exception>
+        /// <returns>Aspect Proxy ServiceProvider</returns>
+        /// <example>
+        /// IServiceCollection services = new ServiceCollection();
+        /// services.AddAspectLite();
+        /// IServiceProvider proxyServiceProvider = AspectServiceProviderFactory.Create(services.BuildServiceProvider());
+        /// </example>
         public static IServiceProvider Create(IServiceProvider serviceProvider)
         {
             return new ProxyServiceProvider(serviceProvider);
