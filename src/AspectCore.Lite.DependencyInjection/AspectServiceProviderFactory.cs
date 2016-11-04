c073a82d368efeb04124b7b724e0174580802b0f
liuhaoyang <liuhaoyang1221@hotmail.com>
2016-11-04 19:10:38 +0800

    add AspectServiceProviderFactory

---
+++ src/AspectCore.Lite.DependencyInjection/AspectServiceProviderFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using AspectCore.Lite.DependencyInjection.Internal;
+
+namespace AspectCore.Lite.DependencyInjection
+{
+    public static class AspectServiceProviderFactory
+    {
+        public static IServiceProvider Create(IServiceProvider serviceProvider)
+        {
+            return new ProxyServiceProvider(serviceProvider);
+        }
+    }
+}
