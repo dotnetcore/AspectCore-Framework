45c91ddfb8914cfb821174635e616cc8195cb5f7
liuhaoyang <liuhaoyang1221@hotmail.com>
2016-11-28 23:29:56 +0800

    update AssemblyProduct

--- src/AspectCore.Lite/Properties/AssemblyInfo.cs
+++ src/AspectCore.Lite/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AspectCore.Lite.Abstractions")]
+[assembly: AssemblyProduct("AspectCore.Lite")]
 [assembly: AssemblyTrademark("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
