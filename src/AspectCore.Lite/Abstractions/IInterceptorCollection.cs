4b72485626523954bea5f0481fce5990971b8815
liuhaoyang <liuhaoyang1221@hotmail.com>
2016-12-01 01:59:56 +0800

    add IInterceptorCollection

---
+++ src/AspectCore.Lite/Abstractions/IInterceptorCollection.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace AspectCore.Lite.Abstractions
+{
+    public interface IInterceptorCollection : IEnumerable<IInterceptor>
+    {
+        void Add(Expression<Func<IInterceptor>> factory);
+    }
+}
---
+++ src/AspectCore.Lite/Internal/InterceptorCollection.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using AspectCore.Lite.Abstractions;
+using AspectCore.Lite.Common;
+
+namespace AspectCore.Lite.Internal
+{
+    internal sealed class InterceptorCollection : IInterceptorCollection
+    {
+        private readonly ConcurrentBag<Func<IInterceptor>> interceptorConcurrentBag =
+            new ConcurrentBag<Func<IInterceptor>>();
+
+        public void Add(Expression<Func<IInterceptor>> factory)
+        {
+            ExceptionHelper.ThrowArgumentNull(factory, nameof(factory));
+            interceptorConcurrentBag.Add(factory.Compile());
+        }
+
+        public IEnumerator<IInterceptor> GetEnumerator()
+        {
+            var factoryCollection = interceptorConcurrentBag.ToArray();
+            foreach (var factory in factoryCollection)
+            {
+                var interceptor = factory();
+                if (interceptor != null) yield return interceptor;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
