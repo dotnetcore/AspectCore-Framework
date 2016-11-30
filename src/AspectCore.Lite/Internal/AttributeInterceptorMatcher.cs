8ed11bd03183c780b9cc66ef5270d37f02ecbe50
liuhaoyang <liuhaoyang1221@hotmail.com>
2016-12-01 02:01:23 +0800

    add InterceptorsFilter for AttributeInterceptorMatcher

--- src/AspectCore.Lite/Internal/AttributeInterceptorMatcher.cs
+++ src/AspectCore.Lite/Internal/AttributeInterceptorMatcher.cs
@@ -1,7 +1,7 @@
-﻿using System.Collections.Concurrent;
+﻿using System;
+using System.Collections.Concurrent;
 using AspectCore.Lite.Abstractions;
 using AspectCore.Lite.Common;
-using AspectCore.Lite.Extensions;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
@@ -13,18 +13,11 @@ namespace AspectCore.Lite.Internal
         private static readonly ConcurrentDictionary<MethodInfo, IInterceptor[]> AttributeInterceptorCache =
             new ConcurrentDictionary<MethodInfo, IInterceptor[]>();
 
-        private static IEnumerable<IInterceptor> InterceptorsIterator(MethodInfo methodInfo, TypeInfo typeInfo)
+        private readonly IInterceptorCollection interceptorCollection;
+
+        public AttributeInterceptorMatcher(IInterceptorCollection interceptorCollection)
         {
-            foreach (var attribute in typeInfo.GetCustomAttributes())
-            {
-                var interceptor = attribute as IInterceptor;
-                if (interceptor != null) yield return interceptor;
-            }
-            foreach (var attribute in methodInfo.GetCustomAttributes())
-            {
-                var interceptor = attribute as IInterceptor;
-                if (interceptor != null) yield return interceptor;
-            }
+            this.interceptorCollection = interceptorCollection;
         }
 
         public IInterceptor[] Match(MethodInfo method, TypeInfo typeInfo)
@@ -33,9 +26,47 @@ namespace AspectCore.Lite.Internal
             ExceptionHelper.ThrowArgumentNull(typeInfo, nameof(typeInfo));
             return AttributeInterceptorCache.GetOrAdd(method, key =>
             {
-                var interceptorAttributes = InterceptorsIterator(method, typeInfo);
-                return interceptorAttributes.Distinct(i => i.GetType()).OrderBy(i => i.Order).ToArray();
+                var interceptorAttributes = InterceptorsIterator(method, typeInfo, interceptorCollection);
+                return InterceptorsFilter(interceptorAttributes).OrderBy(i => i.Order).ToArray();
             });
+        }
+
+        private static IEnumerable<IInterceptor> InterceptorsIterator(
+            MethodInfo methodInfo, TypeInfo typeInfo, IEnumerable<IInterceptor> interceptorCollection)
+        {
+            foreach (var attribute in methodInfo.GetCustomAttributes())
+            {
+                var interceptor = attribute as IInterceptor;
+                if (interceptor != null) yield return interceptor;
+            }
+            foreach (var attribute in typeInfo.GetCustomAttributes())
+            {
+                var interceptor = attribute as IInterceptor;
+                if (interceptor != null) yield return interceptor;
+            }
+            foreach (var interceptor in interceptorCollection)
+            {
+                yield return interceptor;
+            }
+        }
+
+        private static IEnumerable<IInterceptor> InterceptorsFilter(IEnumerable<IInterceptor> interceptors)
+        {
+            var set = new HashSet<Type>();
+            foreach (var interceptor in interceptors)
+            {
+                if (interceptor.AllowMultiple)
+                {
+                    yield return interceptor;
+                }
+                else
+                {
+                    if (set.Add(interceptor.GetType()))
+                    {
+                        yield return interceptor;
+                    }
+                }
+            }
         }
     }
 }
