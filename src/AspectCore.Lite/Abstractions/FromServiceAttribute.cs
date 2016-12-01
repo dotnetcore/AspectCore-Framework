7f03b0aa2b38c6f3cc026ed0c910adcea9cfce9c
liuhaoyang <liuhaoyang1221@hotmail.com>
2016-12-01 18:14:06 +0800

    move FromServiceAttribute

--- src/AspectCore.Lite/Abstractions/FromServiceAttribute.cs
+++ src/AspectCore.Lite/Abstractions/FromServiceAttribute.cs
@@ -2,6 +2,7 @@
 
 namespace AspectCore.Lite.Abstractions
 {
+    [NonAspect]
     [AttributeUsage(AttributeTargets.Property | AttributeTargets.Parameter, AllowMultiple = false, Inherited = false)]
     public sealed class FromServiceAttribute : Attribute
     {
