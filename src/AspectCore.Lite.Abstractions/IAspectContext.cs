bf7b621671ab3ff8ec11e27e42fce4665a7869bd
liuhaoyang <liuhaoyang1221@hotmail.com>
2016-12-03 01:36:54 +0800

    Add IAspectContext

---
+++ src/AspectCore.Lite.Abstractions/IAspectContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AspectCore.Lite.Abstractions
+{
+    public interface IAspectContext : IDisposable
+    {
+        IServiceProvider ServiceProvider { get; }
+
+        TargetDescriptor Target { get; }
+
+        ProxyDescriptor Proxy { get; }
+
+        ParameterCollection Parameters { get; }
+
+        ParameterDescriptor ReturnParameter { get; }
+
+        Object AspectData { get; set; }
+    }
+}
