64a25c3aaef0c7b6077f496955878278c93a711c
liuhaoyang <liuhaoyang1221@hotmail.com>
2016-12-03 01:01:04 +0800

    rename Pxory to ProxyDescriptor

---
+++ src/AspectCore.Lite.Abstractions/ProxyDescriptor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection;
+
+namespace AspectCore.Lite.Abstractions
+{
+    public sealed class ProxyDescriptor
+    {
+        public object ProxyInstance { get; }
+        public MethodInfo ProxyMethod { get; }
+        public Type ProxyType { get; set; }
+
+        public ProxyDescriptor(object proxyInstance, MethodInfo proxyMethod, Type proxyType)
+        {
+            if (proxyInstance == null)
+            {
+                throw new ArgumentNullException(nameof(proxyInstance));
+            }
+            if (proxyMethod == null)
+            {
+                throw new ArgumentNullException(nameof(proxyMethod));
+            }
+            if (proxyType == null)
+            {
+                throw new ArgumentNullException(nameof(proxyType));
+            }
+
+            ProxyType = proxyType;
+            ProxyMethod = proxyMethod;
+            ProxyInstance = proxyInstance;
+        }
+    }
+}
