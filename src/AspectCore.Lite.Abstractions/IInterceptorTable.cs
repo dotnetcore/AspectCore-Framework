445bfc173e9db03522d4369edb1d3e8fd105b348
liuhaoyang <liuhaoyang1221@hotmail.com>
2016-12-03 13:15:12 +0800

    Add IInterceptorTable for global interceptor

---
+++ src/AspectCore.Lite.Abstractions/IInterceptorTable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AspectCore.Lite.Abstractions
+{
+    [NonAspect]
+    public interface IInterceptorTable : IEnumerable<IInterceptor>
+    {
+        void Add(Type interceptorType, params object[] args);
+    }
+}
