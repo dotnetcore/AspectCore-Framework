b80177732dd5e63a59ba0822d37d75b73b9bc42b
liuhaoyang <liuhaoyang1221@hotmail.com>
2016-12-03 13:08:45 +0800

    Add IAspectActivator to activation aspect

---
+++ src/AspectCore.Lite.Abstractions/IAspectActivator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Reflection;
+
+namespace AspectCore.Lite.Abstractions
+{
+    [NonAspect]
+    public interface IAspectActivator
+    {
+        void InitializationMetaData(Type serviceType, MethodInfo serviceMethod, MethodInfo targetMethod, MethodInfo proxyMethod);
+
+        T Invoke<T>(object targetInstance, object proxyInstance, params object[] paramters);
+
+        T InvokeAsync<T>(object targetInstance, object proxyInstance, params object[] paramters);
+    }
+}
