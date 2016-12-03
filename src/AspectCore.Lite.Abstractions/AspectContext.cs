7eca91d4822e2a7f5ee4bf8f05e5ae18826ef27a
liuhaoyang <liuhaoyang1221@hotmail.com>
2016-12-03 12:45:03 +0800

    change AspectContext to abstract class

---
+++ src/AspectCore.Lite.Abstractions/AspectContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace AspectCore.Lite.Abstractions
+{
+    public abstract class AspectContext
+    {
+        public virtual IServiceProvider ServiceProvider { get; }
+
+        public virtual TargetDescriptor Target { get; }
+
+        public virtual ProxyDescriptor Proxy { get; }
+
+        public virtual ParameterCollection Parameters { get; }
+
+        public virtual ParameterDescriptor ReturnParameter { get; }
+
+        public virtual Object AspectData { get; set; }
+
+        public AspectContext(IServiceProvider serviceProvider, TargetDescriptor target, ProxyDescriptor proxy, ParameterCollection parameters, ReturnParameterDescriptor returnParameter)
+        {
+            if (target == null)
+            {
+                throw new ArgumentNullException(nameof(target));
+            }
+            if (proxy == null)
+            {
+                throw new ArgumentNullException(nameof(proxy));
+            }
+            if (parameters == null)
+            {
+                throw new ArgumentNullException(nameof(parameters));
+            }
+            if (returnParameter == null)
+            {
+                throw new ArgumentNullException(nameof(returnParameter));
+            }
+
+            ServiceProvider = serviceProvider;
+            Target = target;
+            Proxy = proxy;
+            Parameters = parameters;
+            ReturnParameter = returnParameter;
+        }
+    }
+}
