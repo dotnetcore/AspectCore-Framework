380bb047d38d011a8d95f6940c168f38bc9e5020
liuhaoyang <liuhaoyang1221@hotmail.com>
2016-12-07 22:59:06 +0800

    Add AspectCore.Lite.Abstractions/ITargetServiceProvider.cs

---
+++ src/AspectCore.Lite.Abstractions/ITargetServiceProvider.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace AspectCore.Lite.Abstractions
+{
+    [NonAspect]
+    public interface ITargetServiceProvider
+    {
+        object GetTarget(Type serviceType);
+    }
+}
