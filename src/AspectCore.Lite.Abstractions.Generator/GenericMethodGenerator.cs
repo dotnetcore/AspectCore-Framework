7d5c1629583d8b93f2136af2507d806b3649e899
liuhaoyang <liuhaoyang1221@hotmail.com>
2016-12-09 00:38:36 +0800

    Add GenericMethodGenerator

---
+++ src/AspectCore.Lite.Abstractions.Generator/GenericMethodGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection.Emit;
+
+namespace AspectCore.Lite.Abstractions.Generator
+{
+    public abstract class GenericMethodGenerator : MethodGenerator
+    {
+        public abstract bool IsGenericMethod { get; }
+
+        public GenericMethodGenerator(TypeBuilder declaringMember) : base(declaringMember)
+        {
+        }
+
+        protected override MethodBuilder Accept(GeneratorVisitor visitor)
+        {
+            var methodBuilder = base.Accept(visitor);
+            if (IsGenericMethod)
+            {
+                GeneratingGenericParameter(methodBuilder);
+            }
+            return methodBuilder;
+        }
+
+        protected abstract void GeneratingGenericParameter(MethodBuilder declaringMethod);
+    }
+}
