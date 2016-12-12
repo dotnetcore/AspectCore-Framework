911fa42a57f1b9d58dc64d0e62c374ecfd30e38d
liuhaoyang <liuhaoyang1221@hotmail.com>
2016-12-12 21:22:45 +0800

    clear AspectCore.Lite.DynamicProxy

--- src/AspectCore.Lite.DynamicProxy/InjectedAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace AspectCore.Lite.DynamicProxy
-{
-    [AttributeUsage(AttributeTargets.Parameter | AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
-    public sealed class InjectedAttribute : Attribute
-    {
-    }
-}
--- src/AspectCore.Lite.DynamicProxy/Resolution/Generators/AspectGeneratorVisitor.cs
+++
@@ -1,8 +0,0 @@
-﻿using AspectCore.Lite.Abstractions.Generator;
-
-namespace AspectCore.Lite.DynamicProxy.Resolution.Generators
-{
-    internal class AspectGeneratorVisitor : GeneratorVisitor
-    {
-    }
-}
