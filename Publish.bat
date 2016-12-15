c9f257efec1a23b4fea49199b52499bd9fd1875f
liuhaoyang <liuhaoyang1221@hotmail.com>
2016-12-15 23:40:36 +0800

    update Publish.bat

--- Publish.bat
+++ Publish.bat
@@ -1,12 +1,15 @@
 @echo off
+for /R "packages" %%s in (*) do (
+del %%s
+)
 dotnet pack src/AspectCore.Lite.Abstractions --configuration Release --output packages
-dotnet pack src/AspectCore.Lite.Abstractions.Generator --configuration Release --output ../packages
+dotnet pack src/AspectCore.Lite.Abstractions.Generator --configuration Release --output packages
 dotnet pack src/AspectCore.Lite.Abstractions.Resolution --configuration Release --output packages
-dotnet pack src/AspectCore.Lite.DynamicProxy --configuration Release --output ../packages
+dotnet pack src/AspectCore.Lite.DynamicProxy --configuration Release --output packages
 
 set /p key=input key:
 
-for /R "../packages" %%s in (*symbols.nupkg) do ( 
+for /R "packages" %%s in (*symbols.nupkg) do ( 
 call nuget push %%s -s http://servicepackages.chinacloudsites.cn/nuget %key%
 )
 
