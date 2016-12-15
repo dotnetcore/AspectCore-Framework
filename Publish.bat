479bb9caeaf45a95bf905b14373da50b3b7da718
liuhaoyang <liuhaoyang1221@hotmail.com>
2016-12-15 23:13:47 +0800

    Add publish.bat to push packages to vnext nuget server

---
+++ Publish.bat
@@ -0,0 +1,13 @@
+@echo off
+dotnet pack src/AspectCore.Lite.Abstractions --configuration Release --output packages
+dotnet pack src/AspectCore.Lite.Abstractions.Generator --configuration Release --output ../packages
+dotnet pack src/AspectCore.Lite.Abstractions.Resolution --configuration Release --output packages
+dotnet pack src/AspectCore.Lite.DynamicProxy --configuration Release --output ../packages
+
+set /p key=input key:
+
+for /R "../packages" %%s in (*symbols.nupkg) do ( 
+call nuget push %%s -s http://servicepackages.chinacloudsites.cn/nuget %key%
+)
+
+pause
