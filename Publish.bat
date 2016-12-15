4d75e70b76a1bb27598617e08a7e141400a3609f
liuhaoyang <liuhaoyang1221@hotmail.com>
2016-12-16 00:03:32 +0800

    update Publish.bat to check packages exist

--- Publish.bat
+++ Publish.bat
@@ -1,16 +1,23 @@
 @echo off
-for /R "packages" %%s in (*) do (
-del %%s
+
+if not exist packages (
+    md packages
 )
+
+for /R "packages" %%s in (*) do (
+    del %%s
+)
+
 dotnet pack src/AspectCore.Lite.Abstractions --configuration Release --output packages
 dotnet pack src/AspectCore.Lite.Abstractions.Generator --configuration Release --output packages
 dotnet pack src/AspectCore.Lite.Abstractions.Resolution --configuration Release --output packages
 dotnet pack src/AspectCore.Lite.DynamicProxy --configuration Release --output packages
 
 set /p key=input key:
+set source=http://servicepackages.chinacloudsites.cn/nuget
 
 for /R "packages" %%s in (*symbols.nupkg) do ( 
-call nuget push %%s -s http://servicepackages.chinacloudsites.cn/nuget %key%
+    call nuget push %%s -s %source% %key%
 )
 
 pause
