27ac7baba76238cd5e1348f890626adeca940166
liuhaoyang <liuhaoyang1221@hotmail.com>
2016-12-16 16:13:29 +0800

    Add

---
+++ src/AspectCore.Lite.Abstractions/IAspectConfiguratorFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspectCore.Lite.Abstractions
+{
+    [NonAspect]
+    public interface IAspectConfiguratorFactory<TContainer>
+    {
+        IAspectConfigurator CreateConfigurator(TContainer container);
+    }
+}
