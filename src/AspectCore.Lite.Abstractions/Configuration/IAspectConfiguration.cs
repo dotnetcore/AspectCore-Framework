aa67734a9f7c528df2e596198c13fac94552302d
liuhaoyang <liuhaoyang1221@hotmail.com>
2017-01-06 21:26:23 +0800

    Add AspectConfigurationExtensions

--- src/AspectCore.Lite.Abstractions/Configuration/IAspectConfiguration.cs
+++ src/AspectCore.Lite.Abstractions/Configuration/IAspectConfiguration.cs
@@ -7,10 +7,6 @@ namespace AspectCore.Lite.Abstractions
     [NonAspect]
     public interface IAspectConfiguration
     {
-        //void Use(Func<MethodInfo, IInterceptor> configure);
-
-        //void Ignore(Func<MethodInfo, bool> configure);
-
-        IConfigurationOption<TOption> GetConfiguration<TOption>();
+        IConfigurationOption<TOption> GetConfigurationOption<TOption>();
     }
 }
