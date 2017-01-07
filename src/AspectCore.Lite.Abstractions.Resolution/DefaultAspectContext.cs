efaaa0ad193ba740adfe9c71df94fc44bdde2baf
liuhaoyang <liuhaoyang1221@hotmail.com>
2017-01-07 13:42:41 +0800

    throw NotImplementedException if IServiceProvider is null

--- src/AspectCore.Lite.Abstractions.Resolution/DefaultAspectContext.cs
+++ src/AspectCore.Lite.Abstractions.Resolution/DefaultAspectContext.cs
@@ -5,6 +5,18 @@ namespace AspectCore.Lite.Abstractions.Resolution
 {
     internal sealed class DefaultAspectContext : AspectContext
     {
+        public override IServiceProvider ServiceProvider
+        {
+            get
+            {
+                if(base.ServiceProvider == null)
+                {
+                    throw new NotImplementedException("The current context does not support this call.");
+                }
+
+                return base.ServiceProvider;
+            }
+        }
         public DefaultAspectContext(IServiceProvider serviceProvider, TargetDescriptor target, ProxyDescriptor proxy, ParameterCollection parameters, ReturnParameterDescriptor returnParameter) 
             : base(serviceProvider, target, proxy, parameters, returnParameter)
         {
