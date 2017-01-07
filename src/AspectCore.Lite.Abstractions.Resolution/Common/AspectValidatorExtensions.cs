ab687c8441446d9b17fd3bb4451b3cd4de95af3d
liuhaoyang <liuhaoyang1221@hotmail.com>
2017-01-07 13:34:31 +0800

    Add AspectValidatorExtensions

---
+++ src/AspectCore.Lite.Abstractions.Resolution/Common/AspectValidatorExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace AspectCore.Lite.Abstractions.Resolution.Common
+{
+    public static class AspectValidatorExtensions
+    {
+        public static bool Validate(this IAspectValidator aspectValidator, Type type)
+        {
+            if (aspectValidator == null)
+            {
+                throw new ArgumentNullException(nameof(aspectValidator));
+            }
+            if (type == null)
+            {
+                throw new ArgumentNullException(nameof(type));
+            }
+
+            return Validate(aspectValidator, type.GetTypeInfo());
+        }
+
+        public static bool Validate(this IAspectValidator aspectValidator, TypeInfo typeInfo)
+        {
+            if (aspectValidator == null)
+            {
+                throw new ArgumentNullException(nameof(aspectValidator));
+            }
+            if (typeInfo == null)
+            {
+                throw new ArgumentNullException(nameof(typeInfo));
+            }
+            if (typeInfo.IsValueType)
+            {
+                return false;
+            }
+
+            return typeInfo.DeclaredMethods.Any(method => aspectValidator.Validate(method));
+        }
+    }
+}
--- src/AspectCore.Lite.Abstractions.Resolution/Common/TypeInfoExtensions.cs
+++ src/AspectCore.Lite.Abstractions.Resolution/Common/TypeInfoExtensions.cs
@@ -1,26 +1,10 @@
-﻿using AspectCore.Lite.Abstractions.Resolution.Generators;
-using System;
-using System.Linq;
+﻿using System;
 using System.Reflection;
 
 namespace AspectCore.Lite.Abstractions.Resolution.Common
 {
     public static class TypeInfoExtensions
     {
-        public static bool ValidateAspect(this TypeInfo typeInfo, IAspectValidator aspectValidator)
-        {
-            if (typeInfo == null)
-            {
-                throw new ArgumentNullException(nameof(typeInfo));
-            }
-            if (typeInfo.IsValueType)
-            {
-                return false;
-            }
-
-            return typeInfo.DeclaredMethods.Any(method => aspectValidator.Validate(method));
-        }
-
         public static bool CanInherited(this TypeInfo typeInfo)
         {
             if (typeInfo == null)
