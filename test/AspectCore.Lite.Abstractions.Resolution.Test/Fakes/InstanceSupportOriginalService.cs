54a82774b9ecb0ded55d76bdf7048d5039401121
liuhaoyang <liuhaoyang1221@hotmail.com>
2017-01-10 23:48:17 +0800

    Add AspectContextTest,ConfigurationOptionTest & InterceptorInjectorTest for AspectCore.Lite.Abstractions.Resolution

--- test/AspectCore.Lite.Abstractions.Resolution.Test/Fakes/InstanceSupportOriginalService.cs
+++ test/AspectCore.Lite.Abstractions.Resolution.Test/Fakes/InstanceSupportOriginalService.cs
@@ -1,23 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace AspectCore.Lite.Abstractions.Resolution.Test.Fakes
+﻿namespace AspectCore.Lite.Abstractions.Resolution.Test.Fakes
 {
-    public class InstanceSupportOriginalService: ISupportOriginalService
+    public class InstanceSupportOriginalService: InstanceServiceProvider, ISupportOriginalService
     {
-        private readonly object instance;
+        public InstanceSupportOriginalService(object instance) : base(instance)
+        {
+        }
 
         public void Dispose()
         {
-            var disposable = instance as IDisposable;
-            disposable?.Dispose();
-        }
-
-        public object GetService(Type serviceType)
-        {
-            return instance;
         }
     }
 }
---
+++ test/AspectCore.Lite.Abstractions.Resolution.Test/InterceptorInjectorTest.cs
@@ -0,0 +1,58 @@
+﻿using AspectCore.Lite.Abstractions.Resolution.Test.Fakes;
+using Xunit;
+
+namespace AspectCore.Lite.Abstractions.Resolution.Test
+{
+    public class InterceptorInjectorTest
+    {
+        [Fact]
+        public void Inject_Test()
+        {
+            var configuration = new AspectConfiguration();
+           
+            var interceptor = new InjectedInterceptor();
+
+            Assert.Null(interceptor.Configuration);
+
+            var interceptorInjector = new InterceptorInjector(new InstanceServiceProvider(configuration));
+
+            interceptorInjector.Inject(interceptor);
+
+            Assert.NotNull(interceptor.Configuration);
+
+            Assert.Equal(interceptor.Configuration, configuration);
+        }
+
+        [Fact]
+        public void Inject_NoSetAccessor_Test()
+        {
+            var configuration = new AspectConfiguration();
+
+            var interceptor = new InjectedInterceptor();
+
+            Assert.Null(interceptor.ConfigurationWithNoSet);
+
+            var interceptorInjector = new InterceptorInjector(new InstanceServiceProvider(configuration));
+
+            interceptorInjector.Inject(interceptor);
+
+            Assert.Null(interceptor.ConfigurationWithNoSet);
+        }
+
+        [Fact]
+        public void Inject_NoFromServicesAttribute_Test()
+        {
+            var configuration = new AspectConfiguration();
+
+            var interceptor = new InjectedInterceptor();
+
+            Assert.Null(interceptor.ConfigurationWithNoFromServicesAttribute);
+
+            var interceptorInjector = new InterceptorInjector(new InstanceServiceProvider(configuration));
+
+            interceptorInjector.Inject(interceptor);
+
+            Assert.Null(interceptor.ConfigurationWithNoFromServicesAttribute);
+        }
+    }
+}
