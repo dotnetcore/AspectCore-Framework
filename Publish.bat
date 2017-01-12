f0b5ded9cb89ec2e020e198494a7306fa4db16c3
liuhaoyang <liuhaoyang1221@hotmail.com>
2017-01-13 01:33:28 +0800

    update

--- Publish.bat
+++ Publish.bat
@@ -16,7 +16,7 @@ set /p key=input key:
 set source=http://servicepackages.chinacloudsites.cn/nuget
 
 for /R "packages" %%s in (*symbols.nupkg) do ( 
-    call nuget push %%s -s %source% %key%
+    call nuget push %%s -s %source%  %key%
 )
 
 pause
