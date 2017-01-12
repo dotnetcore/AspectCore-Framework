c165476d0cbfc5aa5fe1be8393b498bcd97d8fe5
liuhaoyang <liuhaoyang1221@hotmail.com>
2017-01-12 17:17:09 +0800

    Add test for DynamicProxy Project

---
+++ test/AspectCore.Lite.DynamicProxy.Test/ServiceProviderTest.cs
@@ -0,0 +1,23 @@
+﻿using AspectCore.Lite.Abstractions;
+using System;
+using Xunit;
+
+namespace AspectCore.Lite.DynamicProxy.Test
+{
+    public class ServiceProviderTest
+    {
+        [Fact]
+        public void ServiceProvider_Test()
+        {
+            IServiceProvider servicePorvider = new ProxyFactoryBuilder().Build().ServiceProvider;
+            Assert.NotNull(servicePorvider.GetService(typeof(IServiceProvider)));
+            Assert.NotNull(servicePorvider.GetService(typeof(IAspectValidator)));
+            Assert.NotNull(servicePorvider.GetService(typeof(IProxyGenerator)));
+            Assert.NotNull(servicePorvider.GetService(typeof(IAspectActivator)));
+            Assert.NotNull(servicePorvider.GetService(typeof(IAspectConfiguration)));
+            Assert.NotNull(servicePorvider.GetService(typeof(IAspectBuilder)));
+            Assert.NotNull(servicePorvider.GetService(typeof(IInterceptorMatcher)));
+            Assert.NotNull(servicePorvider.GetService(typeof(IInterceptorInjector)));
+        }
+    }
+}
