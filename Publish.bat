7238e3fadd7eb67c4a70bdc060c4f45668916321
liuhaoyang <liuhaoyang1221@hotmail.com>
2017-01-14 21:01:09 +0800

    run test pre pack

--- Publish.bat
+++ Publish.bat
@@ -8,6 +8,10 @@ for /R "packages" %%s in (*) do (
     del %%s
 )
 
+dotnet test test/AspectCore.Lite.Abstractions.Test --configuration release
+dotnet test test/AspectCore.Lite.Abstractions.Resolution.Test --configuration release
+dotnet test test/AspectCore.Lite.DynamicProxy.Test --configuration release
+
 dotnet pack src/AspectCore.Lite.Abstractions --configuration Release --output packages
 dotnet pack src/AspectCore.Lite.Abstractions.Resolution --configuration Release --output packages
 dotnet pack src/AspectCore.Lite.DynamicProxy --configuration Release --output packages
