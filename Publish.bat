6326aa524d2f8735b7ddea0cba61fe70695b3fe5
liuhaoyang <liuhaoyang1221@hotmail.com>
2017-01-17 14:22:19 +0800

    remove AspectCore.Lite.DynamicProxy project

--- Publish.bat
+++ Publish.bat
@@ -10,11 +10,9 @@ for /R "packages" %%s in (*) do (
 
 dotnet test test/AspectCore.Lite.Abstractions.Test --configuration release
 dotnet test test/AspectCore.Lite.Abstractions.Resolution.Test --configuration release
-dotnet test test/AspectCore.Lite.DynamicProxy.Test --configuration release
 
 dotnet pack src/AspectCore.Lite.Abstractions --configuration Release --output packages
 dotnet pack src/AspectCore.Lite.Abstractions.Resolution --configuration Release --output packages
-dotnet pack src/AspectCore.Lite.DynamicProxy --configuration Release --output packages
 
 set /p key=input key:
 set source=http://servicepackages.chinacloudsites.cn/nuget
