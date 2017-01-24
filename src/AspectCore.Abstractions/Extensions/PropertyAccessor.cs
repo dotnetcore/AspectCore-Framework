be00baf89f57a5c48b8ab791aa1f888e832ae2ef
liuhaoyang <liuhaoyang1221@hotmail.com>
2017-01-24 21:23:36 +0800

    Add FastGetValue

--- src/AspectCore.Abstractions/Extensions/PropertyAccessor.cs
+++ src/AspectCore.Abstractions/Extensions/PropertyAccessor.cs
@@ -1,31 +1,15 @@
 ﻿using System;
 using System.Collections.Concurrent;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
+using System.Linq.Expressions;
 using System.Reflection;
-using System.Threading.Tasks;
 
 namespace AspectCore.Abstractions.Extensions
 {
     public sealed class PropertyAccessor
     {
-        private delegate TValue ByRefFunc<TDeclaringType, TValue>(ref TDeclaringType arg);
+        private readonly static ConcurrentDictionary<PropertyInfo, Func<object, object>> getterCache = new ConcurrentDictionary<PropertyInfo, Func<object, object>>();
 
-        private static readonly MethodInfo CallPropertyGetterOpenGenericMethod =
-            typeof(PropertyAccessor).GetTypeInfo().GetDeclaredMethod(nameof(CallPropertyGetter));
-
-        private static readonly MethodInfo CallPropertyGetterByReferenceOpenGenericMethod =
-            typeof(PropertyAccessor).GetTypeInfo().GetDeclaredMethod(nameof(CallPropertyGetterByReference));
-
-        private static readonly MethodInfo CallNullSafePropertyGetterOpenGenericMethod =
-            typeof(PropertyAccessor).GetTypeInfo().GetDeclaredMethod(nameof(CallNullSafePropertyGetter));
-
-        private static readonly MethodInfo CallNullSafePropertyGetterByReferenceOpenGenericMethod =
-            typeof(PropertyAccessor).GetTypeInfo().GetDeclaredMethod(nameof(CallNullSafePropertyGetterByReference));
-
-        private static readonly MethodInfo CallPropertySetterOpenGenericMethod =
-            typeof(PropertyAccessor).GetTypeInfo().GetDeclaredMethod(nameof(CallPropertySetter));
+        private readonly static ConcurrentDictionary<PropertyInfo, Action<object, object>> setterCache = new ConcurrentDictionary<PropertyInfo, Action<object, object>>();
 
         private readonly PropertyInfo property;
 
@@ -39,124 +23,36 @@ namespace AspectCore.Abstractions.Extensions
             this.property = property;
         }
 
-        /// <summary>
-
-        public Func<object, object> MakeFastPropertyGetter(PropertyInfo propertyInfo)
+        public Func<object, object> CreatePropertyGetter()
         {
-            Debug.Assert(propertyInfo != null);
-
-            return MakeFastPropertyGetter(
-                propertyInfo,
-                CallPropertyGetterOpenGenericMethod,
-                CallPropertyGetterByReferenceOpenGenericMethod);
+            return getterCache.GetOrAdd(property, MakeFastPropertyGetter);
         }
 
-        private Func<object, object> MakeFastPropertyGetter( PropertyInfo propertyInfo,
-            MethodInfo propertyGetterWrapperMethod,
-            MethodInfo propertyGetterByRefWrapperMethod)
+        public Action<object,object> CreatePropertySetter()
         {
-            var getMethod = propertyInfo.GetMethod;
-
-            if (getMethod.DeclaringType.GetTypeInfo().IsValueType)
-            {
-                return MakeFastPropertyGetter(
-                    typeof(ByRefFunc<,>),
-                    getMethod,
-                    propertyGetterByRefWrapperMethod);
-            }
-            else
-            {
-                return MakeFastPropertyGetter(
-                    typeof(Func<,>),
-                    getMethod,
-                    propertyGetterWrapperMethod);
-            }
+            return setterCache.GetOrAdd(property, MakeFastPropertySetter);
         }
 
-        private static Func<object, object> MakeFastPropertyGetter(Type openGenericDelegateType, MethodInfo propertyGetMethod, MethodInfo openGenericWrapperMethod)
+        private Func<object, object> MakeFastPropertyGetter(PropertyInfo propertyInfo)
         {
-            var typeInput = propertyGetMethod.DeclaringType;
-            var typeOutput = propertyGetMethod.ReturnType;
-
-            var delegateType = openGenericDelegateType.MakeGenericType(typeInput, typeOutput);
-            var propertyGetterDelegate = propertyGetMethod.CreateDelegate(delegateType);
-
-            var wrapperDelegateMethod = openGenericWrapperMethod.MakeGenericMethod(typeInput, typeOutput);
-            var accessorDelegate = wrapperDelegateMethod.CreateDelegate(
-                typeof(Func<object, object>),
-                propertyGetterDelegate);
-
-            return (Func<object, object>)accessorDelegate;
+            var instanceParam = Expression.Parameter(typeof(object), "instance");
+            var castInstanceExpression = Expression.Convert(instanceParam, propertyInfo.DeclaringType);
+            var getter = propertyInfo.GetGetMethod();
+            var val = Expression.Call(castInstanceExpression, getter);
+            var lambdaExpression = Expression.Lambda<Func<object, object>>(Expression.Convert(val, typeof(object)), instanceParam);
+            return lambdaExpression.Compile();
         }
 
-        private  Action<object, object> MakeFastPropertySetter(PropertyInfo propertyInfo)
+        private Action<object, object> MakeFastPropertySetter(PropertyInfo propertyInfo)
         {
-
-            var setMethod = propertyInfo.SetMethod;
-            var parameters = setMethod.GetParameters();
-
-            var typeInput = setMethod.DeclaringType;
-            var parameterType = parameters[0].ParameterType;
-
-            var propertySetterAsAction =
-                setMethod.CreateDelegate(typeof(Action<,>).MakeGenericType(typeInput, parameterType));
-            var callPropertySetterClosedGenericMethod =
-                CallPropertySetterOpenGenericMethod.MakeGenericMethod(typeInput, parameterType);
-            var callPropertySetterDelegate =
-                callPropertySetterClosedGenericMethod.CreateDelegate(
-                    typeof(Action<object, object>), propertySetterAsAction);
-
-            return (Action<object, object>)callPropertySetterDelegate;
-        }
-
-
-        private static object CallPropertyGetter<TDeclaringType, TValue>(
-            Func<TDeclaringType, TValue> getter,
-            object target)
-        {
-            return getter((TDeclaringType)target);
-        }
-
-        private static object CallPropertyGetterByReference<TDeclaringType, TValue>(
-            ByRefFunc<TDeclaringType, TValue> getter,
-            object target)
-        {
-            var unboxed = (TDeclaringType)target;
-            return getter(ref unboxed);
-        }
-
-        private static object CallNullSafePropertyGetter<TDeclaringType, TValue>(
-            Func<TDeclaringType, TValue> getter,
-            object target)
-        {
-            if (target == null)
-            {
-                return null;
-            }
-
-            return getter((TDeclaringType)target);
-        }
-
-
-        private static object CallNullSafePropertyGetterByReference<TDeclaringType, TValue>(
-            ByRefFunc<TDeclaringType, TValue> getter,
-            object target)
-        {
-            if (target == null)
-            {
-                return null;
-            }
-
-            var unboxed = (TDeclaringType)target;
-            return getter(ref unboxed);
-        }
-
-        private static void CallPropertySetter<TDeclaringType, TValue>(
-            Action<TDeclaringType, TValue> setter,
-            object target,
-            object value)
-        {
-            setter((TDeclaringType)target, (TValue)value);
+            var instanceParam = Expression.Parameter(typeof(object), "instance");
+            var valueParam = Expression.Parameter(typeof(object), "value");
+            var castInstanceExpression = Expression.Convert(instanceParam, propertyInfo.DeclaringType);
+            var castValueExpression = Expression.Convert(valueParam, propertyInfo.PropertyType);
+            var setter = propertyInfo.SetMethod;
+            var assignExpression = Expression.Call(castInstanceExpression, setter, castValueExpression);
+            var lambdaExpression = Expression.Lambda<Action<object, object>>(assignExpression, instanceParam, valueParam);
+            return lambdaExpression.Compile();
         }
     }
 }
---
+++ src/AspectCore.Abstractions/Extensions/PropertyInfoExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Reflection;
+
+namespace AspectCore.Abstractions.Extensions
+{
+    public static class PropertyInfoExtensions
+    {
+        public static object FastGetValue(this PropertyInfo property, object instance)
+        {
+            if (property == null)
+            {
+                throw new ArgumentNullException(nameof(property));
+            }
+            if (instance == null)
+            {
+                throw new ArgumentNullException(nameof(instance));
+            }
+
+            return new PropertyAccessor(property).CreatePropertyGetter()(instance);
+        }
+
+        public static TReturn FastGetValue<TReturn>(this PropertyInfo property, object instance)
+        {
+            return (TReturn)FastGetValue(property, instance);
+        }
+
+        public static void FastSetValue(this PropertyInfo property, object instance, object value)
+        {
+            if (property == null)
+            {
+                throw new ArgumentNullException(nameof(property));
+            }
+            if (instance == null)
+            {
+                throw new ArgumentNullException(nameof(instance));
+            }
+
+            new PropertyAccessor(property).CreatePropertySetter()(instance, value);
+        }
+    }
+}
