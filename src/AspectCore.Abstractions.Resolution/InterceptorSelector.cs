a9d8566d912c70e09af0caf509fd59a5616a071f
liuhaoyang <liuhaoyang1221@hotmail.com>
2017-01-27 17:57:27 +0800

    add IInterceptorSelector

---
+++ src/AspectCore.Abstractions.Resolution/InterceptorSelector.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace AspectCore.Abstractions.Resolution
+{
+    public sealed class InterceptorSelector : IInterceptorSelector
+    {
+        private readonly IInterceptorMatcher interceptorMatcher;
+        private readonly IInterceptorInjectorProvider interceptorInjectorProvider;
+
+        public InterceptorSelector(
+          IInterceptorMatcher interceptorMatcher,
+          IInterceptorInjectorProvider interceptorInjectorProvider)
+        {
+            if (interceptorMatcher == null)
+            {
+                throw new ArgumentNullException(nameof(interceptorMatcher));
+            }
+            if (interceptorInjectorProvider == null)
+            {
+                throw new ArgumentNullException(nameof(interceptorInjectorProvider));
+            }
+            this.interceptorMatcher = interceptorMatcher;
+            this.interceptorInjectorProvider = interceptorInjectorProvider;
+        }
+
+        public IInterceptor[] Select(MethodInfo method)
+        {
+            if (method == null)
+            {
+                throw new ArgumentNullException(nameof(method));
+            }
+
+            return interceptorMatcher.Match(method, method.DeclaringType.GetTypeInfo()).
+                 Select(i =>
+                 {
+                     interceptorInjectorProvider.GetInjector(i.GetType()).Inject(i);
+                     return i;
+                 }).ToArray();
+        }
+    }
+}
---
+++ src/AspectCore.Abstractions/IInterceptorSelector.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+
+namespace AspectCore.Abstractions
+{
+    [NonAspect]
+    public interface IInterceptorSelector
+    {
+        IInterceptor[] Select(MethodInfo method);
+    }
+}
