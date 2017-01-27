22ae4205d25cdefd4f8eeae81bd8b896ea42ea66
liuhaoyang <liuhaoyang1221@hotmail.com>
2017-01-27 17:56:29 +0800

    update

--- Publish.bat
+++ Publish.bat
@@ -14,10 +14,14 @@ dotnet test test/AspectCore.Abstractions.Resolution.Test --configuration Release
 dotnet pack src/AspectCore.Abstractions --configuration Release --output packages
 dotnet pack src/AspectCore.Abstractions.Resolution --configuration Release --output packages
 
+for /R "packages" %%s in (*symbols.nupkg) do (
+    del %%s
+)
+
 set /p key=input key:
 set source=https://www.myget.org/F/aspectcore/api/v2/package
 
-for /R "packages" %%s in (*symbols.nupkg) do ( 
+for /R "packages" %%s in (*.nupkg) do ( 
     call nuget push %%s -s %source%  %key%
 )
 
