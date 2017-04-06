a71be44c38b32f62fc52650a55cd0787eb640c03
liuhaoyang <liuhaoyang1221@hotmail.com>
2017-04-06 23:43:37 +0800

    add AspectValidationHandler

---
+++ src/AspectCore.Abstractions/Internal/ValidationHandler/DynamicallyAspectValidationHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using AspectCore.Abstractions.Extensions;
+
+namespace AspectCore.Abstractions.Internal
+{
+    public class DynamicallyAspectValidationHandler : IAspectValidationHandler
+    {
+        public int Order { get; } = 1;
+
+        public bool Invoke(MethodInfo method, AspectValidationDelegate next)
+        {
+            if (method.DeclaringType.GetTypeInfo().IsDynamically())
+            {
+                return false;
+            }
+            return next(method);
+        }
+    }
+}
