43d84327b4650163078498904423df1effe518a9
liuhaoyang <liuhaoyang1221@hotmail.com>
2017-04-07 00:17:02 +0800

    Add AccessibleAspectValidationHandler

---
+++ src/AspectCore.Abstractions/Internal/ValidationHandler/AccessibleAspectValidationHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+
+namespace AspectCore.Abstractions.Internal.ValidationHandler
+{
+    public class AccessibleAspectValidationHandler : IAspectValidationHandler
+    {
+        public int Order { get; } = 3;
+
+        public bool Invoke(MethodInfo method, AspectValidationDelegate next)
+        {
+            var declaringType = method.DeclaringType.GetTypeInfo();
+            if (!IsAccessibility(declaringType) || !IsAccessibility(method))
+            {
+                return false;
+            }
+            return next(method);
+        }
+
+        private bool IsAccessibility(TypeInfo declaringType)
+        {
+            return !(declaringType.IsNotPublic || declaringType.IsValueType || declaringType.IsSealed || !declaringType.IsNestedPublic);
+        }
+
+        private bool IsAccessibility(MethodInfo method)
+        {
+            return !method.IsStatic && !method.IsFinal && method.IsVirtual && (method.IsPublic || method.IsFamily || method.IsFamilyOrAssembly);
+        }
+    }
+}
