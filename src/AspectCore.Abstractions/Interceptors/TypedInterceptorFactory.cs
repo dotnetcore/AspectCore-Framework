e54f80754a56664ce2155921380a6a26ec4049a0
liuhaoyang <liuhaoyang1221@hotmail.com>
2017-06-10 19:41:56 +0800

    fix TypedInterceptorFactory

--- src/AspectCore.Abstractions/Interceptors/TypedInterceptorFactory.cs
+++ src/AspectCore.Abstractions/Interceptors/TypedInterceptorFactory.cs
@@ -6,7 +6,8 @@ namespace AspectCore.Abstractions
 {
     public class TypedInterceptorFactory : IInterceptorFactory
     {
-        private static readonly Predicate<MethodInfo>[] Empty = new Predicate<MethodInfo>[0];
+        private static readonly Predicate<MethodInfo>[] EmptyPredicates = new Predicate<MethodInfo>[0];
+        private static readonly object[] EmptyArgs = new object[0];
 
         private readonly ICollection<Predicate<MethodInfo>> _predicates;
 
@@ -15,26 +16,22 @@ namespace AspectCore.Abstractions
         public Type InterceptorType { get; }
 
         public TypedInterceptorFactory(Type interceptorType)
-            : this(interceptorType, null, Empty)
+            : this(interceptorType, null, EmptyPredicates)
         {
         }
 
         public TypedInterceptorFactory(Type interceptorType, object[] args)
-            : this(interceptorType, args, Empty)
+            : this(interceptorType, args, EmptyPredicates)
         {
         }
 
         public TypedInterceptorFactory(Type interceptorType, Predicate<MethodInfo>[] predicates)
-          : this(interceptorType, null, predicates)
+          : this(interceptorType, EmptyArgs, predicates)
         {
         }
 
         public TypedInterceptorFactory(Type interceptorType, object[] args, Predicate<MethodInfo>[] predicates)
         {
-            if (predicates == null)
-            {
-                throw new ArgumentNullException(nameof(predicates));
-            }
             if (interceptorType == null)
             {
                 throw new ArgumentNullException(nameof(interceptorType));
@@ -44,8 +41,8 @@ namespace AspectCore.Abstractions
                 throw new ArgumentException($"{interceptorType} is not an interceptor type.", nameof(interceptorType));
             }
             InterceptorType = interceptorType;
-            Args = args ?? new object[0];
-            _predicates = predicates;
+            Args = args ?? EmptyArgs;
+            _predicates = predicates ?? EmptyPredicates;
         }
 
         public IInterceptor CreateInstance(IServiceProvider serviceProvider)
@@ -56,6 +53,10 @@ namespace AspectCore.Abstractions
 
         public bool CanCreated(MethodInfo method)
         {
+            if (_predicates.Count == 0)
+            {
+                return true;
+            }
             foreach (var predicate in _predicates)
             {
                 if (predicate(method)) return true;
