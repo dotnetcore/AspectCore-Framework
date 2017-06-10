c132cb116c3f29c9acab1504be8fe946b01511a7
liuhaoyang <liuhaoyang1221@hotmail.com>
2017-06-10 22:35:07 +0800

    Assembly signature

--- Publish.bat
+++ Publish.bat
@@ -10,8 +10,8 @@ for /R "packages" %%s in (*) do (
 
 rem dotnet test test/AspectCore.Abstractions.Test --configuration Release
 
-dotnet pack src/AspectCore.Abstractions --configuration Release --output packages
-dotnet pack src/AspectCore.Core --configuration Release --output packages
+dotnet pack src/AspectCore.Abstractions --configuration Release --output ../../packages
+dotnet pack src/AspectCore.Core --configuration Release --output ../../packages
 
 for /R "packages" %%s in (*symbols.nupkg) do (
     del %%s
@@ -20,8 +20,10 @@ for /R "packages" %%s in (*symbols.nupkg) do (
 set /p key=input key:
 set source=https://www.myget.org/F/aspectcore/api/v2/package
 
+call nuget.exe setApiKey %key% -Source %source%  
+rem using nuget v4.x
 for /R "packages" %%s in (*.nupkg) do ( 
-    call nuget push %%s -s %source%  %key%
+    call nuget.exe push %%s -Source %source%  
 )
 
 pause
