d069c77532f8676ba9448f30b8cebc1bc5b590be
Lemon <liuhaoyang1221@hotmail.com>
2017-07-16 10:01:15 +0800

    Add ScopedContext project

---
+++ src/AspectCore.Extensions.ScopedContext/IAspectContextScheduler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AspectCore.Abstractions;
+
+namespace AspectCore.Extensions.ScopedContext
+{
+    [NonAspect]
+    public interface IAspectContextScheduler
+    {
+        bool TryEnter(AspectContext context);
+
+        void Release(AspectContext context);
+
+        bool TryInclude(AspectContext context, IInterceptor interceptor);
+
+        AspectContext[] GetCurrentContexts();
+    }
+}
