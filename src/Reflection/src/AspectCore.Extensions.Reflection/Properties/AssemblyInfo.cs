ebcc0fe58297fa27d5e2c9925ccd059c093f1ec3
Lemon <liuhaoyang1221@hotmail.com>
2017-08-02 17:27:23 +0800

    fix : Attempted to read or write protected memory.

--- src/Reflection/src/AspectCore.Extensions.Reflection/Properties/AssemblyInfo.cs
+++ src/Reflection/src/AspectCore.Extensions.Reflection/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AspectCore.Extensions.Reflection")]
+[assembly: AssemblyProduct("AspectCore Framework")]
 [assembly: AssemblyTrademark("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
