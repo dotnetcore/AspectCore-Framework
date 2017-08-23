6c62467aec2666c284b41b947159316113ce6aa3
Lemon <liuhaoyang1221@hotmail.com>
2017-08-23 22:55:07 +0800

    Add ResolveEnumerable

--- src/Abstractions/src/AspectCore.Core/Injectors/InitialServiceResolver.cs
+++ src/Abstractions/src/AspectCore.Core/Injectors/InitialServiceResolver.cs
@@ -1,13 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using AspectCore.Abstractions;
-using System.Collections.Concurrent;
 
 namespace AspectCore.Core.Injectors
 {
-    internal sealed class ServiceTable
+    internal sealed class InitialServiceResolver
     {
-       
     }
 }
