a889aa6b9a627501a49b373a44d9e644e3fe993c
Lemon <liuhaoyang1221@hotmail.com>
2017-08-24 23:33:57 +0800

    Add ProxyServiceDefinition

--- src/Abstractions/src/AspectCore.Core/Injectors/InitialServiceResolver.cs
+++ src/Abstractions/src/AspectCore.Core/Injectors/InitialServiceResolver.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Collections.Concurrent;
+using AspectCore.Abstractions;
 
 namespace AspectCore.Core.Injectors
 {
     internal sealed class InitialServiceResolver
     {
+        private readonly ConcurrentDictionary<Type, LinkedList<ProxyServiceDefinition>> proxyServiceDefinitions = new ConcurrentDictionary<Type, LinkedList<ProxyServiceDefinition>>();
     }
 }
---
+++ src/Abstractions/src/AspectCore.Core/Injectors/ProxyServiceDefinition.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AspectCore.Abstractions;
+
+namespace AspectCore.Core.Injectors
+{
+    internal class ProxyServiceDefinition : ServiceDefinition
+    {
+        public ServiceDefinition ServiceDefinition { get; }
+
+        public ProxyServiceDefinition(ServiceDefinition serviceDefinition) : base(serviceDefinition.ServiceType, serviceDefinition.Lifetime)
+        {
+        }
+    }
+}
