23f2f1501b5335344094768caf58c17acf58b61e
Lemon <liuhaoyang1221@hotmail.com>
2017-09-10 12:33:48 +0800

    Add sample/AspectCore.Extensions.DependencyInjection.Sample

---
+++ src/Extras/sample/AspectCore.Extensions.DependencyInjection.Sample/DynamicProxy/ServiceExecuteLoggerInterceptor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using AspectCore.DynamicProxy;
+using AspectCore.Injector;
+using Microsoft.Extensions.Logging;
+
+namespace AspectCore.Extensions.DependencyInjection.Sample.DynamicProxy
+{
+    public class ServiceExecuteLoggerInterceptor : InterceptorAttribute
+    {
+        [FromContainer]
+        public ILogger<ServiceExecuteLoggerInterceptor> Logger { get; set; }
+
+        public async override Task Invoke(AspectContext context, AspectDelegate next)
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            await next(context);
+            stopwatch.Stop();
+            Logger?.LogInformation("Executed method {0}.{1}.{2} ({3}) in {4}ms", 
+                context.ProxyMethod.DeclaringType.Namespace, 
+                context.ProxyMethod.DeclaringType.Name,
+                context.ProxyMethod.Name,
+                context.ProxyMethod.DeclaringType.Assembly.GetName().Name,
+                stopwatch.ElapsedMilliseconds
+                );
+        }
+    }
+}
