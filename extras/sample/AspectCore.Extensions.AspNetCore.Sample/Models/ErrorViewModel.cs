a358b9378ddad1819a31078315a46195f1769a79
liuhaoyang <liuhaoyang@taolx.com>
2017-09-27 17:59:48 +0800

    add AspectCore.Extensions.AspNetCore.Sample

---
+++ extras/sample/AspectCore.Extensions.AspNetCore.Sample/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspectCore.Extensions.AspNetCore.Sample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
