818650f5accde46efd2bfca019335937b09ddae5
Lemon <liuhaoyang1221@hotmail.com>
2017-10-26 21:56:04 +0800

    fix ConfigureInterceptorSelector

--- core/src/AspectCore.Core/DynamicProxy/ConfigureInterceptorSelector.cs
+++ core/src/AspectCore.Core/DynamicProxy/ConfigureInterceptorSelector.cs
@@ -21,10 +21,19 @@ namespace AspectCore.DynamicProxy
         public IEnumerable<IInterceptor> Select(MethodInfo method)
         {
             //todo fix nonaspect
-            return _aspectConfiguration.Interceptors
-                .Where(x => x.CanCreated(method))
-                .Select(x => x.CreateInstance(_serviceProvider))
-                .OfType<IInterceptor>();
+            foreach (var interceptorFactory in _aspectConfiguration.Interceptors)
+            {
+                if (interceptorFactory.Predicates.Length != 0)
+                {
+                    if (interceptorFactory.CanCreated(method))
+                        yield return interceptorFactory.CreateInstance(_serviceProvider);
+                }
+                else
+                {
+                    if (!_aspectConfiguration.NonAspectPredicates.Any(x => x(method)))
+                        yield return interceptorFactory.CreateInstance(_serviceProvider);
+                }
+            }
         }
     }
 }
---
+++ core/test/AspectCore.Tests/DynamicProxy/ConfigureInterceptorSelectorTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading.Tasks;
+using AspectCore.Configuration;
+using AspectCore.DynamicProxy;
+using Xunit;
+
+namespace AspectCore.Tests.DynamicProxy
+{
+    public class ConfigureInterceptorSelectorTest : DynamicProxyTestBase
+    {
+        [Fact]
+        public void Selector_Test()
+        {
+            var service = ProxyGenerator.CreateClassProxy<FakeSelectService>();
+            var result = service.GetVal(0);
+            Assert.Equal(1, result);
+        }
+        
+        protected override void Configure(IAspectConfiguration configuration)
+        {
+            configuration.Interceptors.AddTyped<Intercept1>(Predicates.ForNameSpace("*"));
+            configuration.Interceptors.AddTyped<Intercept2>();
+        }
+
+        private class Intercept1 : AbstractInterceptor
+        {
+            public override async Task Invoke(AspectContext context, AspectDelegate next)
+            {
+                await context.Invoke(next);
+                var result = (int) context.ReturnValue;
+                context.ReturnValue = result + 1;
+            }
+        }
+
+        private class Intercept2 : AbstractInterceptor
+        {
+            public override async Task Invoke(AspectContext context, AspectDelegate next)
+            {
+                await context.Invoke(next);
+                var result = (int) context.ReturnValue;
+                context.ReturnValue = result + 2;
+            }
+        }
+    }
+}
+
+namespace AspectCore.DynamicProxy
+{
+    public class FakeSelectService
+    {
+        public virtual int GetVal(int val)
+        {
+            return val;
+        }
+    }
+}
+
