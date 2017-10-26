c0c8167b8981859b3caee3068c3674beedb8a5aa
Lemon <liuhaoyang1221@hotmail.com>
2017-10-26 17:06:16 +0800

    Add AddAspectScope extensions

---
+++ extras/src/AspectCore.Extensions.AspNetCore/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AspectCore.DynamicProxy;
+using AspectCore.Extensions.AspectScope;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AspectCore.Extensions.AspNetCore
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddAspectScope(this IServiceCollection services)
+        {
+            if (services == null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+            services.AddScoped<IAspectScheduler, ScopeAspectScheduler>();
+            services.AddScoped<IAspectContextFactory, ScopeAspectContextFactory>();
+            services.AddScoped<IAspectBuilderFactory, ScopeAspectBuilderFactory>();
+            return services;
+        }
+    }
+}
