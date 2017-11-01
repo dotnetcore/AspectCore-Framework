8937e789f6e76c19ec4f08eafa03cf53540b153b
Lemon <liuhaoyang1221@hotmail.com>
2017-11-01 18:28:11 +0800

    add redisprofiler project

---
+++ redisprofiler/src/AspectCore.Extensions.RedisProfiler/RedisDatabaseProfilerInterceptor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using AspectCore.DynamicProxy;
+
+namespace AspectCore.Extensions.RedisProfiler
+{
+    public class RedisDatabaseProfilerInterceptor : AbstractInterceptor
+    {
+        public override Task Invoke(AspectContext context, AspectDelegate next)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
