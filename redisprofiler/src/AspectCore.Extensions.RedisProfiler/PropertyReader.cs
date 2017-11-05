e1e82785aad19b4dabfbfa14fa1c9992ba123910
Lemon <liuhaoyang1221@hotmail.com>
2017-11-05 11:03:27 +0800

    add PropertyReader

---
+++ redisprofiler/src/AspectCore.Extensions.RedisProfiler/PropertyReader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Reflection;
+using AspectCore.Extensions.Reflection;
+
+namespace AspectCore.Extensions.RedisProfiler
+{
+    class PropertyReader
+    {
+        private readonly ConcurrentDictionary<string, PropertyReflector> _reflectorCache;
+        private readonly Type _instanceType;
+
+        public PropertyReader(Type type)
+        {
+            _instanceType = type;
+            _reflectorCache = new ConcurrentDictionary<string, PropertyReflector>();
+        }
+
+        public object GetValue(object instance, string propertyName)
+        {
+            if (propertyName == null)
+            {
+                throw new ArgumentNullException(nameof(propertyName));
+            }
+            var reflector = _reflectorCache.GetOrAdd(propertyName, name => _instanceType.GetTypeInfo().GetProperty(name)?.GetReflector());
+            return reflector?.GetValue(instance);
+        }
+    }
+}
