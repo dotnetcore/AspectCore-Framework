df68037f8ef00f7e60135b3186d6ec566e92574b
lemon <liuhaoyang1221@hotmail.com>
2018-02-22 18:20:36 +0800

    Add AspectCore.Extensions.Hosting

---
+++ extras/src/AspectCore.Extensions.Hosting/HostBuilderExtensions.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using AspectCore.Extensions.DependencyInjection;
+using AspectCore.Injector;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace AspectCore.Extensions.Hosting
+{
+    public static class HostBuilderExtensions
+    {
+        public static IHostBuilder UseAspectCore(this IHostBuilder hostBuilder, Action<IServiceContainer> configureDelegate)
+        {
+            if (hostBuilder == null)
+            {
+                throw new ArgumentNullException(nameof(hostBuilder));
+            }
+            hostBuilder.UseAspectCore();
+            if (configureDelegate != null)
+            {
+                hostBuilder.ConfigureContainer(configureDelegate);
+            }
+            return hostBuilder;
+        }
+
+        public static IHostBuilder UseAspectCore(this IHostBuilder hostBuilder, Action<IServiceCollection> configureDelegate)
+        {
+            if (hostBuilder == null)
+            {
+                throw new ArgumentNullException(nameof(hostBuilder));
+            }
+            hostBuilder.UseAspectCore();
+            if (configureDelegate != null)
+            {
+                hostBuilder.ConfigureContainer(configureDelegate);
+            }
+            return hostBuilder;
+        }
+
+        public static IHostBuilder UseAspectCore(this IHostBuilder hostBuilder, Action<HostBuilderContext, IServiceContainer> configureDelegate)
+        {
+            if (hostBuilder == null)
+            {
+                throw new ArgumentNullException(nameof(hostBuilder));
+            }
+            hostBuilder.UseAspectCore();
+            if (configureDelegate != null)
+            {
+                hostBuilder.ConfigureContainer(configureDelegate);
+            }
+            return hostBuilder;
+        }
+
+        public static IHostBuilder UseAspectCore(this IHostBuilder hostBuilder, Action<HostBuilderContext, IServiceCollection> configureDelegate)
+        {
+            if (hostBuilder == null)
+            {
+                throw new ArgumentNullException(nameof(hostBuilder));
+            }
+            hostBuilder.UseAspectCore();
+            if (configureDelegate != null)
+            {
+                hostBuilder.ConfigureContainer(configureDelegate);
+            }
+            return hostBuilder;
+        }
+
+        public static IHostBuilder UseAspectCore(this IHostBuilder hostBuilder)
+        {
+            return hostBuilder.UseServiceProviderFactory(new AspectCoreServiceProviderFactory());
+        }
+    }
+}
