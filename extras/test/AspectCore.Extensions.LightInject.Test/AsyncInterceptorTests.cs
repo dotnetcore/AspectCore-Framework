c1efe511918202002a4fd6ab38884f02f458cc78
huoshan12345 <89009143@qq.com>
2019-02-01 17:36:15 +0800

    add interceptor and dynamicProxy support for LightInject

---
+++ extras/test/AspectCore.Extensions.LightInject.Test/AsyncInterceptorTests.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using AspectCore.Extensions.LightInject;
+using LightInject;
+using Xunit;
+
+namespace AspectCoreTest.LightInject
+{
+    public class AsyncService
+    {
+        [AsyncIncreament]
+        public virtual void DonotGet(int num)
+        {
+        }
+
+        [AsyncIncreament]
+        public virtual Task DonotGetAsync(int num)
+        {
+            return Task.CompletedTask;
+        }
+
+        [AsyncIncreament]
+        public virtual int Get(int num)
+        {
+            return num;
+        }
+
+        [AsyncIncreament]
+        public virtual async Task<int> GetAsyncWithTask(int num)
+        {
+            await Task.Delay(100);
+            return num;
+        }
+
+        [AsyncIncreament]
+        public virtual async ValueTask<int> GetAsyncWithValueTask(int num)
+        {
+            await Task.Delay(100);
+            return num;
+        }
+    }
+
+    public class AsyncMethodTests
+    {
+        public static IEnumerable<object[]> GetNumbers()
+        {
+            yield return new object[] { 1 };
+            yield return new object[] { 10 };
+            yield return new object[] { 100 };
+        }
+
+        private static IServiceContainer CreateContainer()
+        {
+            var c = new ServiceContainer();
+            c.RegisterDynamicProxy()
+             .Register<AsyncService>();
+            return c;
+        }
+
+        [Theory]
+        [MemberData(nameof(GetNumbers))]
+        public void TestIncreamentForVoid(int input)
+        {
+            var container = CreateContainer();
+            var service = container.GetInstance<AsyncService>();
+            service.DonotGet(input);
+        }
+
+        [Theory]
+        [MemberData(nameof(GetNumbers))]
+        public async Task TestIncreamentForTask(int input)
+        {
+            var container = CreateContainer();
+            var service = container.GetInstance<AsyncService>();
+            await service.DonotGetAsync(input);
+        }
+
+        [Theory]
+        [MemberData(nameof(GetNumbers))]
+        public void TestIncreamentForResult(int input)
+        {
+            var container = CreateContainer();
+            var service = container.GetInstance<AsyncService>();
+            Assert.Equal(input + 1, service.Get(input));
+        }
+
+        [Theory]
+        [MemberData(nameof(GetNumbers))]
+        public async Task TestIncreamentForTaskResult(int input)
+        {
+            var container = CreateContainer();
+            var service = container.GetInstance<AsyncService>();
+            Assert.Equal(input + 1, await service.GetAsyncWithTask(input));
+        }
+
+        [Theory]
+        [MemberData(nameof(GetNumbers))]
+        public async Task TestIncreamentForValueTaskResult(int input)
+        {
+            var container = CreateContainer();
+            var service = container.GetInstance<AsyncService>();
+            Assert.Equal(input + 1, await service.GetAsyncWithValueTask(input));
+        }
+    }
+}
---
+++ extras/test/AspectCore.Extensions.LightInject.Test/AwaitBeforeInvokeNextTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AspectCore.DynamicProxy;
+using AspectCore.Extensions.LightInject;
+using LightInject;
+using Xunit;
+
+namespace AspectCoreTest.LightInject
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class AwaitBeforeInvokeAttribute : AbstractInterceptorAttribute
+    {
+        public override async Task Invoke(AspectContext context, AspectDelegate next)
+        {
+            await Task.Delay(100);
+            if (context.Proxy is AwaitBeforeInvokeTester tester
+                && tester.Cts.IsCancellationRequested)
+            {
+                context.ReturnValue = Task.FromResult(-1);
+                return;
+            }
+            await context.Invoke(next);
+        }
+    }
+
+    public class AwaitBeforeInvokeTester
+    {
+        public CancellationTokenSource Cts { get; set; }
+
+        [AwaitBeforeInvoke]
+        public virtual async Task<int> ExecuteAsync()
+        {
+            await Task.Delay(100);
+            return 1;
+        }
+    }
+
+    public class AwaitBeforeInvokeNextTests
+    {
+        private static IServiceContainer CreateContainer()
+        {
+            var c = new ServiceContainer(new ContainerOptions
+            {
+                DefaultServiceSelector = s => s.Last(),
+                EnablePropertyInjection = false
+            });
+            c.RegisterDynamicProxy()
+                .Register<AwaitBeforeInvokeTester>();
+            return c;
+        }
+
+        [Fact]
+        public async Task Test()
+        {
+            var container = CreateContainer();
+            var service = container.GetInstance<AwaitBeforeInvokeTester>();
+            service.Cts = new CancellationTokenSource(TimeSpan.FromSeconds(10));
+            var result = await service.ExecuteAsync();
+            Assert.Equal(1, result);
+            Assert.False(service.Cts.IsCancellationRequested);
+        }
+    }
+}
