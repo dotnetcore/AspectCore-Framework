680d59dd791099797a036bef353d2f869e39acee
丶亡我之心不死 <kuangLiWen1@live.com>
2019-10-10 17:37:28 +0800

    支持C#8.0的接口默认方法

--- core/src/AspectCore.Core/Utils/ProxyGeneratorUtils.cs
+++ core/src/AspectCore.Core/Utils/ProxyGeneratorUtils.cs
@@ -462,7 +462,25 @@ namespace AspectCore.Utils
                 var implementationMethod = implType.GetTypeInfo().GetMethodBySignature(method);
                 if (implementationMethod == null)
                 {
-                    throw new MissingMethodException($"Type '{implType}' does not contain a method '{method}'.");
+                    var interfaces = implType.GetInterfaces();
+                    if (interfaces == null || interfaces.Length <= 0)
+                    {
+                        throw new MissingMethodException($"Type '{implType}' does not contain a method '{method}'.");
+                    }
+                    var @interface = interfaces.Where(f => f.GetCustomAttribute(typeof(AbstractInterceptorAttribute)) != null).ToArray();
+                    foreach (var item in @interface)
+                    {
+                        implementationMethod = item.GetTypeInfo().GetMethodBySignature(method);
+                        if (implementationMethod != null)
+                        {
+                            break;
+                        }
+                    }
+                    if (implementationMethod == null)
+                    {
+                        throw new MissingMethodException($"Type '{implType}' does not contain a method '{method}'.");
+                    }
+                    //implementationMethod = implType.GetInterfaces()[0].GetTypeInfo().GetMethodBySignature(method);
                 }
 
                 if (method.IsNonAspect())
