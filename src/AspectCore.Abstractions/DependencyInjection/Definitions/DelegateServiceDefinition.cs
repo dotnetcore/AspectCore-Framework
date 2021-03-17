b6ab9008dce3f61d912c6a69898a44cdc3cac5bf
yangjie <huangfeihuong955330@126.com>
2021-03-18 00:11:10 +0800

    添加DelegateServiceDefinition类的注释

--- src/AspectCore.Abstractions/DependencyInjection/Definitions/DelegateServiceDefinition.cs
+++ src/AspectCore.Abstractions/DependencyInjection/Definitions/DelegateServiceDefinition.cs
@@ -2,13 +2,25 @@
 
 namespace AspectCore.DependencyInjection
 {
+    /// <summary>
+    /// 委托类型的服务描述对象
+    /// </summary>
     public class DelegateServiceDefinition : ServiceDefinition
     {
+        /// <summary>
+        /// 构造表示委托类型的服务描述对象的实例
+        /// </summary>
+        /// <param name="serviceType">暴露的服务类型</param>
+        /// <param name="implementationDelegate">一个委托类型</param>
+        /// <param name="lifetime">生命周期</param>
         public DelegateServiceDefinition(Type serviceType, Func<IServiceResolver, object> implementationDelegate, Lifetime lifetime) : base(serviceType, lifetime)
         {
             ImplementationDelegate = implementationDelegate ?? throw new ArgumentNullException(nameof(implementationDelegate));
         }
 
+        /// <summary>
+        /// 此对象关注的委托
+        /// </summary>
         public Func<IServiceResolver, object> ImplementationDelegate { get; }
     }
 }
