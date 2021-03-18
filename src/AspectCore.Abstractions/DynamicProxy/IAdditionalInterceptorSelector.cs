a2a010b3441c6164aa28ef3eceac029de2284e49
yangjie <huangfeihuong955330@126.com>
2021-03-18 17:35:06 +0800

    IAdditionalInterceptorSelector添加注释

--- src/AspectCore.Abstractions/DynamicProxy/IAdditionalInterceptorSelector.cs
+++ src/AspectCore.Abstractions/DynamicProxy/IAdditionalInterceptorSelector.cs
@@ -5,9 +5,18 @@ using System.Text;
 
 namespace AspectCore.DynamicProxy
 {
+    /// <summary>
+    /// 查询继承树上相关的拦截器
+    /// </summary>
     [NonAspect]
     public interface IAdditionalInterceptorSelector
     {
+        /// <summary>
+        /// 根据服务方法和实现方法查询拦截特性
+        /// </summary>
+        /// <param name="serviceMethod">暴露的服务方法</param>
+        /// <param name="implementationMethod">实现方法</param>
+        /// <returns>拦截器集合</returns>
         IEnumerable<IInterceptor> Select(MethodInfo serviceMethod, MethodInfo implementationMethod);
     }
 }
