80240507ead0f7dfa656fdc3921f1faab43e7f3d
yangjie <huangfeihuong955330@126.com>
2021-03-24 16:22:26 +0800

    拦截器工厂的注释

--- src/AspectCore.Core/Configuration/InterceptorFactories/ServiceInterceptorFactory.cs
+++ src/AspectCore.Core/Configuration/InterceptorFactories/ServiceInterceptorFactory.cs
@@ -4,10 +4,18 @@ using AspectCore.DynamicProxy;
 
 namespace AspectCore.Configuration
 {
+    /// <summary>
+    /// 服务拦截器工厂
+    /// </summary>
     public sealed class ServiceInterceptorFactory : InterceptorFactory
     {
         private readonly Type _interceptorType;
 
+        // <summary>
+        /// 此工厂创建的拦截器,由DI激活使用
+        /// </summary>
+        /// <param name="interceptorType">拦截器类型</param>
+        /// <param name="predicates">拦截条件数组（注：为CanCreated方法提供支持以判断是否可以创建此拦截器）</param>
         public ServiceInterceptorFactory(Type interceptorType, params AspectPredicate[] predicates)
             : base(predicates)
         {
@@ -22,6 +30,11 @@ namespace AspectCore.Configuration
             _interceptorType = interceptorType;
         }
 
+        /// <summary>
+        /// 创建由DI激活使用的拦截器
+        /// </summary>
+        /// <param name="serviceProvider">服务提供者</param>
+        /// <returns>拦截器</returns>
         public override IInterceptor CreateInstance(IServiceProvider serviceProvider)
         {
             return new ServiceInterceptorAttribute(_interceptorType);
--- src/AspectCore.Core/Configuration/InterceptorFactories/TypeInterceptorFactory.cs
+++ src/AspectCore.Core/Configuration/InterceptorFactories/TypeInterceptorFactory.cs
@@ -4,12 +4,21 @@ using AspectCore.DynamicProxy;
 
 namespace AspectCore.Configuration
 {
+    /// <summary>
+    /// 类型拦截器工厂
+    /// </summary>
     public sealed class TypeInterceptorFactory : InterceptorFactory
     {
         private readonly static object[] emptyArgs = new object[0];
         private readonly object[] _args;
         private readonly Type _interceptorType;
 
+        /// <summary>
+        /// 构造类型拦截器工厂
+        /// </summary>
+        /// <param name="interceptorType">拦截器类型</param>
+        /// <param name="args">拦截器的构造参数</param>
+        /// <param name="predicates">拦截条件数组（注：为CanCreated方法提供支持以判断是否可以创建此拦截器）</param>
         public TypeInterceptorFactory(Type interceptorType, object[] args, params AspectPredicate[] predicates)
             : base(predicates)
         {
@@ -25,6 +34,11 @@ namespace AspectCore.Configuration
             _args = args ?? emptyArgs;
         }
 
+        /// <summary>
+        /// 创建一个拦截器
+        /// </summary>
+        /// <param name="serviceProvider">服务提供者</param>
+        /// <returns>创建的拦截器</returns>
         public override IInterceptor CreateInstance(IServiceProvider serviceProvider)
         {
             return (IInterceptor)Activator.CreateInstance(_interceptorType, _args);
