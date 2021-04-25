27094d0c6cec6dbbd205c1d11f24702446c57f43
yangjie <huangfeihuong955330@126.com>
2021-04-25 16:16:06 +0800

    add comments for reflector

--- src/AspectCore.Extensions.Reflection/ParameterReflector.cs
+++ src/AspectCore.Extensions.Reflection/ParameterReflector.cs
@@ -4,6 +4,9 @@ using System.Reflection;
 
 namespace AspectCore.Extensions.Reflection
 {
+    /// <summary>
+    /// 参数反射操作
+    /// </summary>
     public partial class ParameterReflector : ICustomAttributeReflectorProvider
     {
         private readonly ParameterInfo _reflectionInfo;
@@ -11,16 +14,35 @@ namespace AspectCore.Extensions.Reflection
 
         public CustomAttributeReflector[] CustomAttributeReflectors => _customAttributeReflectors;
 
+        /// <summary>
+        /// 参数名
+        /// </summary>
         public string Name => _reflectionInfo.Name;
 
+        /// <summary>
+        /// 参数是否有默认值
+        /// </summary>
         public bool HasDeflautValue { get; }
 
+        /// <summary>
+        /// 参数默认值
+        /// </summary>
         public object DefalutValue { get; }
 
+        /// <summary>
+        /// 参数在形参表中的位置（从零开始）
+        /// </summary>
         public int Position { get; }
 
+        /// <summary>
+        /// 参数类型
+        /// </summary>
         public Type ParameterType { get; }
 
+        /// <summary>
+        /// 参数反射操作对象
+        /// </summary>
+        /// <param name="reflectionInfo">参数对象</param>
         private ParameterReflector(ParameterInfo reflectionInfo)
         {
             _reflectionInfo = reflectionInfo ?? throw new ArgumentNullException(nameof(reflectionInfo));
@@ -34,11 +56,19 @@ namespace AspectCore.Extensions.Reflection
             ParameterType = reflectionInfo.ParameterType;
         }
 
+        /// <summary>
+        /// 获取参数对象
+        /// </summary>
+        /// <returns>参数对象</returns>
         public ParameterInfo GetParameterInfo()
         {
             return _reflectionInfo;
         }
 
+        /// <summary>
+        /// 获取友好的字符串表示
+        /// </summary>
+        /// <returns>字符串表示</returns>
         public override string ToString() => $"Parameter : {_reflectionInfo}  ParameterType : {ParameterType}";
     }
 }
--- src/AspectCore.Extensions.Reflection/PropertyReflector.Static.cs
+++ src/AspectCore.Extensions.Reflection/PropertyReflector.Static.cs
@@ -7,13 +7,24 @@ namespace AspectCore.Extensions.Reflection
 {
     public partial class PropertyReflector
     {
+        /// <summary>
+        /// 静态属性反射操作
+        /// </summary>
         private class StaticPropertyReflector : PropertyReflector
         {
+            /// <summary>
+            /// 静态属性反射操作
+            /// </summary>
+            /// <param name="reflectionInfo">属性对象</param>
             public StaticPropertyReflector(PropertyInfo reflectionInfo)
                 : base(reflectionInfo)
             {
             }
 
+            /// <summary>
+            /// 创建一个代表属性get访问器方法的委托
+            /// </summary>
+            /// <returns>代表属性get访问器方法的委托</returns>
             protected override Func<object, object> CreateGetter()
             {
                 var dynamicMethod = new DynamicMethod($"getter-{Guid.NewGuid()}", typeof(object), new Type[] { typeof(object) }, _reflectionInfo.Module, true);
@@ -25,6 +36,10 @@ namespace AspectCore.Extensions.Reflection
                 return (Func<object, object>)dynamicMethod.CreateDelegate(typeof(Func<object, object>));
             }
 
+            /// <summary>
+            /// 创建一个代表属性set访问器方法的委托
+            /// </summary>
+            /// <returns>代表属性set访问器方法的委托</returns>
             protected override Action<object, object> CreateSetter()
             {
                 var dynamicMethod = new DynamicMethod($"setter-{Guid.NewGuid()}", typeof(void), new Type[] { typeof(object), typeof(object) }, _reflectionInfo.Module, true);
@@ -36,21 +51,39 @@ namespace AspectCore.Extensions.Reflection
                 return (Action<object, object>)dynamicMethod.CreateDelegate(typeof(Action<object, object>));
             }
 
+            /// <summary>
+            /// 获取属性值
+            /// </summary>
+            /// <param name="instance">静态属性内部传递null</param>
+            /// <returns>属性值</returns>
             public override object GetValue(object instance)
             {
                 return _getter(null);
             }
 
+            /// <summary>
+            /// 设置属性值
+            /// </summary>
+            /// <param name="instance">静态属性内部传递null</param>
+            /// <param name="value">值</param>
             public override void SetValue(object instance, object value)
             {
                 _setter(null, value);
             }
 
+            /// <summary>
+            /// 获取静态属性值
+            /// </summary>
+            /// <returns>值</returns>
             public override object GetStaticValue()
             {
                 return _getter(null);
             }
 
+            /// <summary>
+            /// 设置静态属性值
+            /// </summary>
+            /// <param name="value">值</param>
             public override void SetStaticValue(object value)
             {
                 _setter(null, value);
