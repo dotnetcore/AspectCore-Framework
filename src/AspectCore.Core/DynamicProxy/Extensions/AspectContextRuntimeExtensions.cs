659b8c71f04b0ccc2dac2876397f26f254343104
huoshan12345 <huoshan12345@hotmail.com>
2021-07-21 17:46:49 +0800

    fix

--- src/AspectCore.Core/DynamicProxy/Extensions/AspectContextRuntimeExtensions.cs
+++ src/AspectCore.Core/DynamicProxy/Extensions/AspectContextRuntimeExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Concurrent;
+using System.Linq.Expressions;
 using System.Reflection;
 using System.Threading.Tasks;
 using AspectCore.Extensions.Reflection;
@@ -12,6 +13,10 @@ namespace AspectCore.DynamicProxy
         private static readonly ConcurrentDictionary<MethodInfo, bool> isAsyncCache = new ConcurrentDictionary<MethodInfo, bool>();
 
         internal static readonly ConcurrentDictionary<MethodInfo, MethodReflector> reflectorTable = new ConcurrentDictionary<MethodInfo, MethodReflector>();
+
+        private static readonly ConcurrentDictionary<TypeInfo, Func<object, object>> _resultFuncCache = new ConcurrentDictionary<TypeInfo, Func<object, object>>();
+
+        private static readonly ConcurrentDictionary<TypeInfo, Func<object, Task>> _asTaskFuncCache = new ConcurrentDictionary<TypeInfo, Func<object, Task>>();
 
         public static ValueTask AwaitIfAsync(this AspectContext aspectContext)
         {
@@ -31,11 +36,14 @@ namespace AspectCore.DynamicProxy
                     await valueTask;
                     break;
                 default:
-                    if (returnValue.GetType().GetTypeInfo().IsValueTaskWithResult())
+                {
+                    var type = returnValue.GetType().GetTypeInfo();
+                    if (type.IsValueTaskWithResult())
                     {
-                        await (dynamic)returnValue;
+                        await ValueTaskWithResultToTask(returnValue, type);
                     }
                     break;
+                }
             }
         }
 
@@ -87,6 +95,56 @@ namespace AspectCore.DynamicProxy
             return Unwrap(returnValue, returnTypeInfo);
         }
 
+        // value should be ValueTask<T>
+        private static Task ValueTaskWithResultToTask(object value, TypeInfo valueTypeInfo)
+        {
+            // NOTE: if we use "await (dynamic)value" to await a ValueTask<T> when T is non-public, we will get an RuntimeBinderException that says
+            // 'System.ValueType' does not contain a definition for 'GetAwaiter'.
+            // So we have to convert ValueTask<T> to Task and then await it.
+            // Please fix this logic if there is a better solution.
+            var func = _asTaskFuncCache.GetOrAdd(valueTypeInfo, k =>
+            {
+                var parameter = Expression.Parameter(typeof(object), "type");
+                var convertedParameter = Expression.Convert(parameter, valueTypeInfo);
+                var method = valueTypeInfo.GetMethod(nameof(ValueTask<int>.AsTask));
+                var property = Expression.Call(convertedParameter, method);
+                var convertedProperty = Expression.Convert(property, typeof(Task));
+                var exp = Expression.Lambda<Func<object, Task>>(convertedProperty, parameter);
+                return exp.Compile();
+            });
+            return func(value);
+        }
+
+        // value should be Task<T> or ValueTask<T>
+        private static object GetTaskResult(object value, TypeInfo valueTypeInfo)
+        {
+            // result: if we use "result = (object)(await (dynamic)value)" to get the result of a ValueTask<T> when T is non-public, we will get an RuntimeBinderException that says
+            // 'System.ValueType' does not contain a definition for 'GetAwaiter'.
+            // So after ValueTask<T> is awaited, we use expression with cache to get the value of ValueTask<T>.Result.
+            // The Benchmark can be viewed in GetTaskResultBenchmarks.cs in AspectCore.Extensions.Reflection.Benchmark.
+            // Here is a sample result: 
+            /*
+                |                            Method |         Mean |
+                |---------------------------------- |-------------:|
+                |          GetTaskResult_Reflection |     342.0 ns |
+                | GetTaskResult_ReflectionWithCache |     279.5 ns |
+                |          GetTaskResult_Expression | 224,219.4 ns |
+                | GetTaskResult_ExpressionWithCache |     125.9 ns |
+                |        GetTaskResult_AwaitDynamic |     124.8 ns |
+            */
+            // Please fix this logic if there is a better solution.
+            var func = _resultFuncCache.GetOrAdd(valueTypeInfo, k =>
+            {
+                var parameter = Expression.Parameter(typeof(object), "type");
+                var convertedParameter = Expression.Convert(parameter, valueTypeInfo);
+                var property = Expression.Property(convertedParameter, nameof(Task<int>.Result));
+                var convertedProperty = Expression.Convert(property, typeof(object));
+                var exp = Expression.Lambda<Func<object, object>>(convertedProperty, parameter);
+                return exp.Compile();
+            });
+            return func(value);
+        }
+
         private static async Task<object> Unwrap(object value, TypeInfo valueTypeInfo)
         {
             object result;
@@ -97,13 +155,13 @@ namespace AspectCore.DynamicProxy
             }
             else if (valueTypeInfo.IsTaskWithResult())
             {
-                // Is there better solution to unwrap ?
-                result = (object)(await (dynamic)value);
+                await (Task)value;
+                result = GetTaskResult(value, valueTypeInfo);
             }
             else if (valueTypeInfo.IsValueTaskWithResult())
             {
-                // Is there better solution to unwrap ?
-                result = (object)(await (dynamic)value);
+                await ValueTaskWithResultToTask(value, valueTypeInfo);
+                result = GetTaskResult(value, valueTypeInfo);
             }
             else if (value is Task)
             {
