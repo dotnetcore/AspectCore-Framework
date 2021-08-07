6da8f1a13e3dbec871089acd361b8273352f2e6c
huoshan12345 <huoshan12345@hotmail.com>
2021-08-07 12:54:47 +0800

    add benchmarks for "GetTaskResult"

--- benchmark/AspectCore.Core.Benchmark/Benchmarks/GetTaskResultBenchmarks.cs
+++ benchmark/AspectCore.Core.Benchmark/Benchmarks/GetTaskResultBenchmarks.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Concurrent;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
+using System.Reflection.Emit;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using AspectCore.DynamicProxy;
@@ -24,12 +26,11 @@ namespace AspectCore.Core.Benchmark.Benchmarks
         private static readonly ConcurrentDictionary<TypeInfo, PropertyInfo> _propertyInfos = new();
         private static readonly ConcurrentDictionary<TypeInfo, Func<object, object>> _delegates = new();
 
-        private static async Task<object> GetTaskResult_Reflection(object value, TypeInfo valueTypeInfo)
-        {
-            await (Task)value;
-            var result = valueTypeInfo.GetProperty(nameof(Task<int>.Result)).GetValue(value);
-            return result;
-        }
+        private static readonly MethodInfo _methodOfCreateDelegate = typeof(GetTaskResultBenchmarks).GetMethods(BindingFlags.Static | BindingFlags.NonPublic)
+            .First(m => m.Name == nameof(CreateDelegateToGetProperty) && m.IsGenericMethod);
+        private static readonly ConcurrentDictionary<TypeInfo, Func<object, object>> _dynamicDelegates = new();
+
+        private static readonly ConcurrentDictionary<TypeInfo, Func<object, object>> _dynamicMethods = new();
 
         private static async Task<object> GetTaskResult_ReflectionWithCache(object value, TypeInfo valueTypeInfo)
         {
@@ -45,18 +46,61 @@ namespace AspectCore.Core.Benchmark.Benchmarks
             return result;
         }
 
-        private static async Task<object> GetTaskResult_Expression(object value, TypeInfo valueTypeInfo)
-        {
-            await (Task)value;
-            var func = AspectContextRuntimeExtensions.CreateFuncToGetTaskResult(valueTypeInfo);
-            return func(value);
-        }
-
         private static async Task<object> GetTaskResult_ExpressionWithCache(object value, TypeInfo valueTypeInfo)
         {
             await (Task)value;
             var func = _delegates.GetOrAdd(valueTypeInfo, k => AspectContextRuntimeExtensions.CreateFuncToGetTaskResult(k));
             return func(value);
+        }
+
+        private static Func<object, object> CreateDelegateToGetProperty<T, TProperty>(PropertyInfo property)
+        {
+            var func = (Func<T, TProperty>)Delegate.CreateDelegate(typeof(Func<T, TProperty>), property.GetGetMethod()!);
+            return o => func((T)o);
+        }
+
+        private static Func<object, object> CreateDelegateToGetProperty(TypeInfo valueTypeInfo, string propertyName)
+        {
+            var prop = valueTypeInfo.GetProperty(propertyName);
+            var func = (Func<PropertyInfo, Func<object, object>>)_methodOfCreateDelegate
+                .MakeGenericMethod(valueTypeInfo, prop.PropertyType)
+                .CreateDelegate(typeof(Func<PropertyInfo, Func<object, object>>));
+            return func(prop);
+        }
+
+        private static async Task<object> GetTaskResult_DynamicDelegateWithCache(object value, TypeInfo valueTypeInfo)
+        {
+            await (Task)value;
+            var func = _dynamicDelegates.GetOrAdd(valueTypeInfo, k => CreateDelegateToGetProperty(k, nameof(Task<int>.Result)));
+            return func(value);
+        }
+
+        private static async Task<object> GetTaskResult_DynamicMethodWithCache(object value, TypeInfo valueTypeInfo)
+        {
+            await (Task)value;
+            var func = _dynamicMethods.GetOrAdd(valueTypeInfo, k => CreateDynamicMethodToGetTaskResult(k));
+            return func(value);
+        }
+
+        private static Func<object, object> CreateDynamicMethodToGetTaskResult(TypeInfo valueTypeInfo)
+        {
+            var prop = valueTypeInfo.GetProperty(nameof(Task<int>.Result));
+
+            var method = new DynamicMethod(
+                name: "__IL_GetTaskResult<" + valueTypeInfo.Name + ">",
+                returnType: prop.PropertyType,
+                parameterTypes: new[] { typeof(object) },
+                owner: typeof(object),
+                skipVisibility: true);
+
+            var il = method.GetILGenerator();
+            il.Emit(OpCodes.Ldarg_0);
+            il.Emit(OpCodes.Castclass, valueTypeInfo);
+            il.Emit(OpCodes.Callvirt, prop.GetGetMethod()!);
+            il.Emit(OpCodes.Castclass, typeof(object));
+            il.Emit(OpCodes.Ret);
+            var func = (Func<object, object>)method.CreateDelegate(typeof(Func<object, object>));
+            return func;
         }
 
         public static async Task GetTaskResult_Test(Func<object, TypeInfo, Task<object>> func)
@@ -72,21 +116,9 @@ namespace AspectCore.Core.Benchmark.Benchmarks
         }
 
         [Benchmark]
-        public async Task GetTaskResult_Reflection()
-        {
-            await GetTaskResult_Test((v, t) => GetTaskResult_Reflection(v, t));
-        }
-
-        [Benchmark]
         public async Task GetTaskResult_ReflectionWithCache()
         {
             await GetTaskResult_Test((v, t) => GetTaskResult_ReflectionWithCache(v, t));
-        }
-
-        [Benchmark]
-        public async Task GetTaskResult_Expression()
-        {
-            await GetTaskResult_Test((v, t) => GetTaskResult_Expression(v, t));
         }
 
         [Benchmark]
@@ -95,10 +127,22 @@ namespace AspectCore.Core.Benchmark.Benchmarks
             await GetTaskResult_Test((v, t) => GetTaskResult_ExpressionWithCache(v, t));
         }
 
-        [Benchmark]
+        [Benchmark(Baseline = true)]
         public async Task GetTaskResult_AwaitDynamic()
         {
             await GetTaskResult_Test((v, t) => GetTaskResult_AwaitDynamic(v, t));
+        }
+
+        [Benchmark]
+        public async Task GetTaskResult_DynamicDelegateWithCache()
+        {
+            await GetTaskResult_Test((v, t) => GetTaskResult_DynamicDelegateWithCache(v, t));
+        }
+
+        [Benchmark]
+        public async Task GetTaskResult_DynamicMethodWithCache()
+        {
+            await GetTaskResult_Test((v, t) => GetTaskResult_DynamicMethodWithCache(v, t));
         }
     }
 }
