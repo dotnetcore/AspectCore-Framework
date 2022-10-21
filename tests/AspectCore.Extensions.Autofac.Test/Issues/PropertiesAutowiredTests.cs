b2205def006d8e3aab083d39759261b17ba6a70a
nivalxer <nivalxer@gmail.com>
2022-10-21 10:04:35 +0800

    add more tests

--- tests/AspectCore.Extensions.Autofac.Test/Issues/PropertiesAutowiredTests.cs
+++ tests/AspectCore.Extensions.Autofac.Test/Issues/PropertiesAutowiredTests.cs
@@ -5,6 +5,8 @@ using AspectCore.Extensions.Autofac;
 using AspectCore.Extensions.Test.Fakes;
 using Xunit;
 using static AspectCoreTest.Autofac.Issues.PropertyInjectorProxyWithVirtualTests;
+using System.Threading.Tasks;
+using AspectCore.DynamicProxy;
 
 namespace AspectCoreTest.Autofac.Issues
 {
@@ -13,11 +15,14 @@ namespace AspectCoreTest.Autofac.Issues
         public interface IService
         {
             int Run();
+            string GetString(string key);
+            string GetStringReturnValueModify(string key);
         }
 
         public interface IServiceTwo
         {
             int Run();
+            string GetString(string key);
         }
 
         public class Service : IService
@@ -29,6 +34,18 @@ namespace AspectCoreTest.Autofac.Issues
             {
                 return ServiceTwo.Run();
             }
+
+            [StringParameterIntercept]
+            public string GetString(string key)
+            {
+                return ServiceTwo.GetString(key);
+            }
+
+            [ReturnValueIntercept]
+            public string GetStringReturnValueModify(string key)
+            {
+                return ServiceTwo.GetString(key);
+            }
         }
 
         public class ServiceTwo : IServiceTwo
@@ -36,6 +53,11 @@ namespace AspectCoreTest.Autofac.Issues
             public int Run()
             {
                 return 1024;
+            }
+
+            public string GetString(string key)
+            {
+                return key;
             }
         }
 
@@ -47,6 +69,34 @@ namespace AspectCoreTest.Autofac.Issues
             {
                 return Service.Run();
             }
+
+            public string GetString(string key)
+            {
+                return Service.GetString(key);
+            }
+
+            public string GetStringReturnValueModify(string key)
+            {
+                return Service.GetStringReturnValueModify(key);
+            }
+        }
+
+        public class StringParameterIntercept : AbstractInterceptorAttribute
+        {
+            public override Task Invoke(AspectContext context, AspectDelegate next)
+            {
+                context.Parameters[0] = "EMT";
+                return context.Invoke(next);
+            }
+        }
+
+        public class ReturnValueIntercept : AbstractInterceptorAttribute
+        {
+            public override async Task Invoke(AspectContext context, AspectDelegate next)
+            {
+                await next(context);
+                context.ReturnValue = "Emilia";
+            }
         }
 
         private ContainerBuilder CreateBuilder()
@@ -57,6 +107,9 @@ namespace AspectCoreTest.Autofac.Issues
             });
         }
 
+        /// <summary>
+        /// 多层属性注入测试
+        /// </summary>
         [Fact]
         public void PropertiesAutowiredWithMultilayer_Test()
         {
@@ -67,6 +120,22 @@ namespace AspectCoreTest.Autofac.Issues
             var container = builder.Build();
             var action = container.Resolve<ControllerAction>();
             action.ActionRun();
+        }
+
+        /// <summary>
+        /// 多层属性注入下启用拦截器测试
+        /// </summary>
+        [Fact]
+        public void PropertiesAutowiredWithMultilayerIntercept_Test()
+        {
+            var builder = CreateBuilder();
+            builder.RegisterType<ControllerAction>().PropertiesAutowired();
+            builder.RegisterType<Service>().AsImplementedInterfaces().PropertiesAutowired();
+            builder.RegisterType<ServiceTwo>().AsImplementedInterfaces().PropertiesAutowired();
+            var container = builder.Build();
+            var action = container.Resolve<ControllerAction>();
+            Assert.Equal("EMT", action.GetString("Lion"));
+            Assert.Equal("Emilia", action.GetStringReturnValueModify("Lion"));
         }
     }
 }
