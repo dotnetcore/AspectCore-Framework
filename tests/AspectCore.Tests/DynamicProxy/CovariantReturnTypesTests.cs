3623e9c3249e2179f18dc1b45e5efdc2520b1817
huoshan12345 <huoshan12345@hotmail.com>
2025-07-19 00:32:09 +0800

    add test cases

---
+++ tests/AspectCore.Tests/DynamicProxy/CovariantReturnTypesTests.cs
@@ -0,0 +1,49 @@
+﻿using AspectCore.DynamicProxy;
+using Xunit;
+
+namespace AspectCore.Tests.DynamicProxy;
+
+public class CovariantReturnTypesTests : DynamicProxyTestBase
+{
+    public interface IService
+    {
+        object Method();
+        object Property { get; }
+    }
+
+    public class Service : IService
+    {
+        public virtual object Method() => nameof(Service);
+        public virtual object Property { get; } = nameof(Service);
+    }
+
+    public class CovariantReturnsService : Service
+    {
+        public override string Method() => nameof(CovariantReturnsService);
+        public override string Property { get; } = nameof(CovariantReturnsService);
+    }
+
+    [Fact]
+    public void CreateClassProxy_CovariantReturns_Test()
+    {
+        var service = ProxyGenerator.CreateClassProxy<CovariantReturnsService>();
+        Assert.Equal(nameof(CovariantReturnsService), service.Method());
+        Assert.Equal(nameof(CovariantReturnsService), service.Property);
+    }
+
+    [Fact]
+    public void CreateClassProxy_WithServiceType_CovariantReturns_Test()
+    {
+        var service = ProxyGenerator.CreateClassProxy<Service, CovariantReturnsService>();
+        Assert.Equal(nameof(CovariantReturnsService), service.Method());
+        Assert.Equal(nameof(CovariantReturnsService), service.Property);
+    }
+
+    [Fact]
+    public void CreateInterfaceProxy_CovariantReturns_Test()
+    {
+        var service = ProxyGenerator.CreateInterfaceProxy<IService, CovariantReturnsService>();
+        Assert.Equal(nameof(CovariantReturnsService), service.Method());
+        Assert.Equal(nameof(CovariantReturnsService), service.Property);
+    }
+}
