48987f49b49f5b25ce3ad97130280fe50a673743
Haoyang Liu <liuhaoyang1221@hotmail.com>
2026-07-20 10:08:39 +0800

    test: complete keyed service interception coverage (#391)
    * test: complete keyed service interception coverage
    
    * test: tidy keyed service interception fixtures

--- tests/AspectCore.Extensions.DependencyInjection.Test/KeyedServiceInterceptionTests.cs
+++ tests/AspectCore.Extensions.DependencyInjection.Test/KeyedServiceInterceptionTests.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Threading.Tasks;
 using AspectCore.DependencyInjection;
 using AspectCore.DynamicProxy;
 using Microsoft.Extensions.DependencyInjection;
@@ -14,46 +13,27 @@ namespace AspectCore.Extensions.DependencyInjection.Test
     /// </summary>
     public class KeyedServiceInterceptionTests
     {
-        public class IncrementInterceptor : AbstractInterceptorAttribute
-        {
-            private readonly int _amount;
-
-            public IncrementInterceptor(int amount)
-            {
-                _amount = amount;
-            }
-
-            public override async Task Invoke(AspectContext context, AspectDelegate next)
-            {
-                await context.Invoke(next);
-                if (context.ReturnValue is int value)
-                {
-                    context.ReturnValue = value + _amount;
-                }
-            }
-        }
-
-        public interface IKeyedCounter
-        {
-            int GetBase();
-            int GetIntercepted();
-        }
-
-        public class KeyedCounter : IKeyedCounter
-        {
-            public int GetBase() => 1;
-
-            [IncrementInterceptor(100)]
-            public int GetIntercepted() => 1;
-        }
-
 #if NET8_0_OR_GREATER
+        private static ServiceProvider BuildProvider(
+            Action<IServiceCollection> configureServices,
+            ProxyEngine engine = ProxyEngine.DynamicProxy)
+        {
+            var services = new ServiceCollection();
+            services.ConfigureDynamicProxyEngine(options =>
+            {
+                options.Engine = engine;
+                options.Strict = engine == ProxyEngine.SourceGenerator;
+                options.AllowRuntimeFallback = engine == ProxyEngine.SourceGenerator ? false : (bool?)null;
+            });
+            configureServices(services);
+            return services.BuildDynamicProxyProvider();
+        }
+
         [Fact]
         public void ResolveKeyedService_ThroughServiceResolver_InterceptionIsActive()
         {
-            var services = new ServiceCollection();
-            services.AddKeyedTransient<IKeyedCounter, KeyedCounter>("alpha");
-            var provider = services.BuildDynamicProxyProvider();
+            var provider = BuildProvider(services =>
+                services.AddKeyedTransient<IKeyedCounter, KeyedCounter>("alpha"));
             var resolver = provider.GetRequiredService<IServiceResolver>();
 
             var svc = resolver.GetKeyedService(typeof(IKeyedCounter), "alpha");
@@ -69,10 +49,11 @@ namespace AspectCore.Extensions.DependencyInjection.Test
         [Fact]
         public void ResolveKeyedService_ThroughServiceResolver_MultipleKeysAreIndependent()
         {
-            var services = new ServiceCollection();
-            services.AddKeyedTransient<IKeyedCounter, KeyedCounter>("alpha");
-            services.AddKeyedTransient<IKeyedCounter, KeyedCounter>("beta");
-            var provider = services.BuildDynamicProxyProvider();
+            var provider = BuildProvider(services =>
+            {
+                services.AddKeyedTransient<IKeyedCounter, KeyedCounter>("alpha");
+                services.AddKeyedTransient<IKeyedCounter, KeyedCounter>("beta");
+            });
             var resolver = provider.GetRequiredService<IServiceResolver>();
 
             var alpha = Assert.IsAssignableFrom<IKeyedCounter>(
@@ -91,9 +72,8 @@ namespace AspectCore.Extensions.DependencyInjection.Test
         [Fact]
         public void ResolveKeyedService_ThroughServiceResolver_UnregisteredKeyReturnsNull()
         {
-            var services = new ServiceCollection();
-            services.AddKeyedTransient<IKeyedCounter, KeyedCounter>("alpha");
-            var provider = services.BuildDynamicProxyProvider();
+            var provider = BuildProvider(services =>
+                services.AddKeyedTransient<IKeyedCounter, KeyedCounter>("alpha"));
             var resolver = provider.GetRequiredService<IServiceResolver>();
 
             var result = resolver.GetKeyedService(typeof(IKeyedCounter), "missing");
@@ -103,9 +83,8 @@ namespace AspectCore.Extensions.DependencyInjection.Test
         [Fact]
         public void ResolveRequiredKeyedService_ThroughServiceResolver_InterceptionIsActive()
         {
-            var services = new ServiceCollection();
-            services.AddKeyedTransient<IKeyedCounter, KeyedCounter>("alpha");
-            var provider = services.BuildDynamicProxyProvider();
+            var provider = BuildProvider(services =>
+                services.AddKeyedTransient<IKeyedCounter, KeyedCounter>("alpha"));
             var resolver = provider.GetRequiredService<IServiceResolver>();
 
             var svc = resolver.GetRequiredKeyedService(typeof(IKeyedCounter), "alpha");
@@ -116,8 +95,7 @@ namespace AspectCore.Extensions.DependencyInjection.Test
         [Fact]
         public void ResolveRequiredKeyedService_ThroughServiceResolver_UnregisteredKeyThrows()
         {
-            var services = new ServiceCollection();
-            var provider = services.BuildDynamicProxyProvider();
+            var provider = BuildProvider(_ => { });
             var resolver = provider.GetRequiredService<IServiceResolver>();
 
             Assert.Throws<InvalidOperationException>(() =>
@@ -127,9 +105,8 @@ namespace AspectCore.Extensions.DependencyInjection.Test
         [Fact]
         public void ResolveKeyedService_ThroughServiceResolver_SameAsServiceProviderDirect()
         {
-            var services = new ServiceCollection();
-            services.AddKeyedTransient<IKeyedCounter, KeyedCounter>("alpha");
-            var provider = services.BuildDynamicProxyProvider();
+            var provider = BuildProvider(services =>
+                services.AddKeyedTransient<IKeyedCounter, KeyedCounter>("alpha"));
             var resolver = provider.GetRequiredService<IServiceResolver>();
 
             // Both resolution paths should yield an intercepted proxy.
@@ -139,6 +116,93 @@ namespace AspectCore.Extensions.DependencyInjection.Test
 
             Assert.NotNull(viaProvider);
             Assert.Equal(viaResolver.GetIntercepted(), viaProvider.GetIntercepted());
+        }
+
+        [Fact]
+        public void ResolveKeyedSingleton_ThroughServiceResolver_PreservesLifetimeAndInterception()
+        {
+            var provider = BuildProvider(services =>
+                services.AddKeyedSingleton<IKeyedCounter, KeyedCounter>("alpha"));
+            var resolver = provider.GetRequiredService<IServiceResolver>();
+
+            var first = Assert.IsAssignableFrom<IKeyedCounter>(
+                resolver.GetKeyedService(typeof(IKeyedCounter), "alpha"));
+            var second = Assert.IsAssignableFrom<IKeyedCounter>(
+                resolver.GetKeyedService(typeof(IKeyedCounter), "alpha"));
+
+            Assert.Same(first, second);
+            Assert.Equal(101, first.GetIntercepted());
+        }
+
+        [Fact]
+        public void ResolveKeyedAndNonKeyedServices_ThroughServiceResolver_AreIndependentAndIntercepted()
+        {
+            var provider = BuildProvider(services =>
+            {
+                services.AddTransient<IKeyedCounter, KeyedCounter>();
+                services.AddKeyedTransient<IKeyedCounter, KeyedCounter>("alpha");
+            });
+            var resolver = provider.GetRequiredService<IServiceResolver>();
+
+            var nonKeyed = Assert.IsAssignableFrom<IKeyedCounter>(
+                resolver.GetRequiredService(typeof(IKeyedCounter)));
+            var keyed = Assert.IsAssignableFrom<IKeyedCounter>(
+                resolver.GetRequiredKeyedService(typeof(IKeyedCounter), "alpha"));
+
+            Assert.NotSame(nonKeyed, keyed);
+            Assert.Equal(101, nonKeyed.GetIntercepted());
+            Assert.Equal(101, keyed.GetIntercepted());
+        }
+
+        [Theory]
+        [InlineData(ProxyEngine.DynamicProxy)]
+        [InlineData(ProxyEngine.SourceGenerator)]
+        public void ResolveKeyedTransient_ThroughServiceResolver_InterceptionIsActive_ForBothEngines(ProxyEngine engine)
+        {
+            var provider = BuildProvider(services =>
+                services.AddKeyedTransient<IKeyedCounter, KeyedCounter>("alpha"), engine);
+            var resolver = provider.GetRequiredService<IServiceResolver>();
+
+            var first = Assert.IsAssignableFrom<IKeyedCounter>(
+                resolver.GetKeyedService(typeof(IKeyedCounter), "alpha"));
+            var second = Assert.IsAssignableFrom<IKeyedCounter>(
+                resolver.GetKeyedService(typeof(IKeyedCounter), "alpha"));
+
+            Assert.NotSame(first, second);
+            Assert.Equal(1, first.GetBase());
+            Assert.Equal(101, first.GetIntercepted());
+            Assert.Equal(101, second.GetIntercepted());
+        }
+
+        [Fact]
+        public void ResolveKeyedServices_SourceGenerator_MultipleKeysMixedWithNonKeyedRemainIndependent()
+        {
+            var provider = BuildProvider(services =>
+            {
+                services.AddTransient<IKeyedCounter, KeyedCounter>();
+                services.AddKeyedSingleton<IKeyedCounter, KeyedCounter>("singleton");
+                services.AddKeyedTransient<IKeyedCounter, KeyedCounter>("transient");
+            }, ProxyEngine.SourceGenerator);
+            var resolver = provider.GetRequiredService<IServiceResolver>();
+
+            var nonKeyed = Assert.IsAssignableFrom<IKeyedCounter>(
+                resolver.GetRequiredService(typeof(IKeyedCounter)));
+            var singletonFirst = Assert.IsAssignableFrom<IKeyedCounter>(
+                resolver.GetRequiredKeyedService(typeof(IKeyedCounter), "singleton"));
+            var singletonSecond = Assert.IsAssignableFrom<IKeyedCounter>(
+                resolver.GetRequiredKeyedService(typeof(IKeyedCounter), "singleton"));
+            var transientFirst = Assert.IsAssignableFrom<IKeyedCounter>(
+                resolver.GetRequiredKeyedService(typeof(IKeyedCounter), "transient"));
+            var transientSecond = Assert.IsAssignableFrom<IKeyedCounter>(
+                resolver.GetRequiredKeyedService(typeof(IKeyedCounter), "transient"));
+
+            Assert.NotSame(nonKeyed, singletonFirst);
+            Assert.Same(singletonFirst, singletonSecond);
+            Assert.NotSame(transientFirst, transientSecond);
+            Assert.Equal(101, nonKeyed.GetIntercepted());
+            Assert.Equal(101, singletonFirst.GetIntercepted());
+            Assert.Equal(101, transientFirst.GetIntercepted());
+            Assert.Null(resolver.GetKeyedService(typeof(IKeyedCounter), "missing"));
         }
 #endif
     }
